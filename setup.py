76e812ebacc7255934941553106c94b4ea644ce8
andrea-mucci <81492948+andrea-mucci@users.noreply.github.com>
2021-03-31 18:37:28 +0200

    [skip ci]modified setup.py for markdown support

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_namespace_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.md') as history_file:
     history = history_file.read()
 
 requirements = [ 'aiomisc', 'six', 'avro', 'orjson', 'lmdb' ]
@@ -32,6 +32,7 @@ setup(
     ],
     description="Python Package with common Classes and Utilities used in Minos Microservices",
     install_requires=requirements,
+    long_description_content_type='text/markdown',
     long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='minos_microservice_common',
