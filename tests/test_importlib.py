ad1e3d4a57d233efb5238d1f8f67529bad537070
andrea-mucci <81492948+andrea-mucci@users.noreply.github.com>
2021-03-31 18:30:12 +0200

    added test for importlib
    added a test to importlib to check exceptions

--- tests/test_importlib.py
+++ tests/test_importlib.py
@@ -1,5 +1,6 @@
 import pytest
 
+from minos.common.exceptions import MinosImportException
 from minos.common.importlib import import_module
 
 
@@ -7,3 +8,9 @@ def test_import_module():
     object_class = import_module("tests.ImportedModule.ImportedClassTest")
     instance_class = object_class()
     assert instance_class.return_test_example() == "test passed"
+
+
+def test_import_module_exception():
+    with pytest.raises(MinosImportException):
+        object_class = import_module("tests.ImportedModuleFail.ImportedClassTest")
+        assert True == True
