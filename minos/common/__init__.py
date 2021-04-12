c3d95183660f036542c1d815e698b8a7fabcf879
Sergio García Prado <sergio@garciparedes.me>
2021-04-12 11:26:32 +0200

    ISSUE #9 * Expose `ModelField` at `minos.common` module level.

--- minos/common/__init__.py
+++ minos/common/__init__.py
@@ -1,1 +1,15 @@
+"""
+
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+
+"""
+
 __version__ = '0.0.1.6'
+
+from .model import (
+    ModelField,
+)
