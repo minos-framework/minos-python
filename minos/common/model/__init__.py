b020c8ec080f7af26dff8ac444420596d583fafa
Sergio García Prado <sergio@garciparedes.me>
2021-04-12 16:51:09 +0200

    ISSUE #19 * Rename "aggregate.py" as "model.py".

--- minos/common/model/__init__.py
+++ minos/common/model/__init__.py
@@ -6,7 +6,7 @@ This file is part of minos framework.
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
 
-from .aggregate import (
+from .model import (
     BOOLEAN,
     NULL,
     INT,
