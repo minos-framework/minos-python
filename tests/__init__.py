971436366914990c3870a4c878fe492d6087acaf
Sergio García Prado <sergio@garciparedes.me>
2021-04-15 09:06:18 +0200

    ISSUE #? * Add header on test files.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,9 @@
-"""Unit test package for minos_microservice_common."""
+"""
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+
+Unit test package for minos_microservice_common.
+"""
