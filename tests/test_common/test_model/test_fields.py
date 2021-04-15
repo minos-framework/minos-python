985b422149a81315d44ef44b0eb5a4bf7755c144
Restyled.io <commits@restyled.io>
2021-04-15 16:11:05 +0000

    Restyled by black

--- tests/test_common/test_model/test_fields.py
+++ tests/test_common/test_model/test_fields.py
@@ -18,9 +18,7 @@ from minos.common import (
     ModelField,
     ModelRef,
 )
-from tests.modelClasses import (
-    User,
-)
+from tests.modelClasses import User
 
 
 class TestModelField(unittest.TestCase):
@@ -123,7 +121,7 @@ class TestModelField(unittest.TestCase):
 
     def test_value_unsupported(self):
         with self.assertRaises(MinosTypeAttributeException):
-            ModelField("test", set[int], {3, })
+            ModelField("test", set[int], {3,})
 
     def test_value_setter(self):
         field = ModelField("test", int, 3)
