bfc49f6a2a9eedbaef6fb19d657674cbaf92bdfb
vladyslav-fenchak <vladyslav.fenchak@clariteia.com>
2021-04-19 10:38:58 +0200

    Update broker.py
    Add the base class for Broker Service #62

---
+++ minos/common/broker.py
@@ -0,0 +1,18 @@
+"""
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+"""
+import abc
+
+
+class BrokerBase(abc.ABC):
+    @abc.abstractmethod
+    def _database(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def save(self):
+        raise NotImplementedError
