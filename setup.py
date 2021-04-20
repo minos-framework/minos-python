99b3dddf66068192188260fcb6b21fdc9ad4f406
Restyled.io <commits@restyled.io>
2021-04-20 14:59:38 +0000

    Restyled by black

--- setup.py
+++ setup.py
@@ -4,43 +4,47 @@
 
 from setuptools import setup, find_namespace_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.md') as history_file:
+with open("HISTORY.md") as history_file:
     history = history_file.read()
 
-requirements = [ 'aiomisc', 'six', 'avro', 'orjson', 'lmdb', 'pyyaml', "aiopg" ]
+requirements = ["aiomisc", "six", "avro", "orjson", "lmdb", "pyyaml", "aiopg"]
 
-setup_requirements = ['pytest-runner',]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Andrea Mucci",
-    author_email='andrea@clariteia.com',
-    python_requires='>=3.5',
+    author_email="andrea@clariteia.com",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Python Package with common Classes and Utilities used in Minos Microservices",
     install_requires=requirements,
-    long_description_content_type='text/markdown',
-    long_description=readme + '\n\n' + history,
+    long_description_content_type="text/markdown",
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='minos_microservice_common',
-    name='minos_microservice_common',
-    packages=find_namespace_packages(include=['minos.*']),
+    keywords="minos_microservice_common",
+    name="minos_microservice_common",
+    packages=find_namespace_packages(include=["minos.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    version='0.0.2',
+    version="0.0.2",
     zip_safe=False,
 )
