206c3900ef403031fa83a7cc629b3f6108e76482
Sergio García Prado <sergio.garcia@clariteia.com>
2021-04-20 16:58:02 +0200

    ISSUE #88 * Add `aiopg` as internal dependency.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md') as readme_file:
 with open('HISTORY.md') as history_file:
     history = history_file.read()
 
-requirements = [ 'aiomisc', 'six', 'avro', 'orjson', 'lmdb', 'pyyaml' ]
+requirements = [ 'aiomisc', 'six', 'avro', 'orjson', 'lmdb', 'pyyaml', "aiopg" ]
 
 setup_requirements = ['pytest-runner',]
 
