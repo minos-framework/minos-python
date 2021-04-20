db664682d72daf7266eb631808da57a1373c4405
Restyled.io <commits@restyled.io>
2021-04-20 11:01:41 +0000

    Restyled by black

--- tests/test_broker.py
+++ tests/test_broker.py
@@ -22,11 +22,7 @@ def config():
 
 @pytest.fixture
 def services(config):
-    return [
-        BrokerDatabaseInitializer(
-            config=config
-        )
-    ]
+    return [BrokerDatabaseInitializer(config=config)]
 
 
 @pytest.fixture()
@@ -38,15 +34,13 @@ async def test_if_queue_table_exists(database):
     time.sleep(1)
     cur = await database.cursor()
 
-    await cur.execute(
-        "SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'queue';"
-    )
+    await cur.execute("SELECT 1 FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'queue';")
     ret = []
     async for row in cur:
         ret.append(row)
 
     database.close()
-    assert ret == [(1, )]
+    assert ret == [(1,)]
 
 
 async def test_events_broker_insertion(config, database):
@@ -58,14 +52,13 @@ async def test_events_broker_insertion(config, database):
 
     cur = await database.cursor()
 
-    await cur.execute(
-        "SELECT 1 FROM queue WHERE topic = 'EventBroker' LIMIT 1;")
+    await cur.execute("SELECT 1 FROM queue WHERE topic = 'EventBroker' LIMIT 1;")
     ret = []
     async for row in cur:
         ret.append(row)
 
     database.close()
-    assert ret == [(1, )]
+    assert ret == [(1,)]
 
 
 async def test_commands_broker_insertion(config, database):
@@ -77,14 +70,13 @@ async def test_commands_broker_insertion(config, database):
 
     cur = await database.cursor()
 
-    await cur.execute(
-        "SELECT 1 FROM queue WHERE topic = 'CommandBroker' LIMIT 1;")
+    await cur.execute("SELECT 1 FROM queue WHERE topic = 'CommandBroker' LIMIT 1;")
     ret = []
     async for row in cur:
         ret.append(row)
 
     database.close()
-    assert ret == [(1, )]
+    assert ret == [(1,)]
 
 
 async def test_drop_database(database):
