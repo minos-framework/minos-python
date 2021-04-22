8e445eeee44e3c35c5b2f849c76e8c45c2e6edee
Restyled.io <commits@restyled.io>
2021-04-22 11:21:35 +0000

    Restyled by isort

--- tests/test_common/test_model/test_abc/test_fields.py
+++ tests/test_common/test_model/test_abc/test_fields.py
@@ -19,7 +19,9 @@ from minos.common import (
     ModelField,
     ModelRef,
 )
-from tests.modelClasses import User
+from tests.modelClasses import (
+    User,
+)
 
 
 class TestModelField(unittest.TestCase):
