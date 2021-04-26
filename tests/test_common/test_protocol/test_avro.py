f29d15f4826850fec3dd6718995d14c2bddf9e9e
Sergio García Prado <sergio.garcia@clariteia.com>
2021-04-26 12:24:01 +0200

    ISSUE #54 * Migrate plain tests to the "unittest.TesCase" class pattern.

--- tests/test_common/test_protocol/test_avro.py
+++ tests/test_common/test_protocol/test_avro.py
@@ -5,105 +5,107 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
+import unittest
 
-import pytest
 from minos.common import (
     MinosAvroProtocol,
     MinosAvroValuesDatabase,
 )
 
 
-def test_encoder_decoder_with_body_dict():
-    headers = {"id": 123, "action": "get"}
-    body = {"message": "this is a message"}
-    data_return_bytes = MinosAvroProtocol.encode(headers, body)
-    assert isinstance(data_return_bytes, bytes) == True
+class TestMinosAvroProtocol(unittest.TestCase):
 
-    # decode
-    returned_dict = MinosAvroProtocol.decode(data_return_bytes)
-    assert returned_dict["headers"]["action"] == "get"
-    assert returned_dict["headers"]["id"] == 123
-    assert returned_dict["body"]["message"] == "this is a message"
+    def test_encoder_decoder_with_body_dict(self):
+        headers = {"id": 123, "action": "get"}
+        body = {"message": "this is a message"}
+        data_return_bytes = MinosAvroProtocol.encode(headers, body)
+        self.assertEqual(True, isinstance(data_return_bytes, bytes))
 
+        # decode
+        returned_dict = MinosAvroProtocol.decode(data_return_bytes)
+        self.assertEqual("get", returned_dict["headers"]["action"])
+        self.assertEqual(123, returned_dict["headers"]["id"])
+        self.assertEqual("this is a message", returned_dict["body"]["message"])
 
-def test_encoder_decoder_with_body_string():
-    headers = {"id": 123, "action": "get"}
-    body = "this is a message"
-    data_return_bytes = MinosAvroProtocol.encode(headers, body)
-    assert isinstance(data_return_bytes, bytes) == True
+    def test_encoder_decoder_with_body_string(self):
+        headers = {"id": 123, "action": "get"}
+        body = "this is a message"
+        data_return_bytes = MinosAvroProtocol.encode(headers, body)
+        self.assertEqual(True, isinstance(data_return_bytes, bytes))
 
-    # decode
-    returned_dict = MinosAvroProtocol.decode(data_return_bytes)
-    assert returned_dict["headers"]["action"] == "get"
-    assert returned_dict["headers"]["id"] == 123
-    assert returned_dict["body"] == "this is a message"
+        # decode
+        returned_dict = MinosAvroProtocol.decode(data_return_bytes)
+        self.assertEqual("get", returned_dict["headers"]["action"])
+        self.assertEqual(123, returned_dict["headers"]["id"])
+        self.assertEqual("this is a message", returned_dict["body"])
 
+    def test_encoder_decoder_with_body_int(self):
+        headers = {"id": 123, "action": "get"}
+        body = 222
+        data_return_bytes = MinosAvroProtocol.encode(headers, body)
+        self.assertEqual(True, isinstance(data_return_bytes, bytes))
 
-def test_encoder_decoder_with_body_int():
-    headers = {"id": 123, "action": "get"}
-    body = 222
-    data_return_bytes = MinosAvroProtocol.encode(headers, body)
-    assert isinstance(data_return_bytes, bytes) == True
+        # decode
+        returned_dict = MinosAvroProtocol.decode(data_return_bytes)
+        self.assertEqual("get", returned_dict["headers"]["action"])
+        self.assertEqual(123, returned_dict["headers"]["id"])
+        self.assertEqual(222, returned_dict["body"])
 
-    # decode
-    returned_dict = MinosAvroProtocol.decode(data_return_bytes)
-    assert returned_dict["headers"]["action"] == "get"
-    assert returned_dict["headers"]["id"] == 123
-    assert returned_dict["body"] == 222
+    def test_encoder_decoder_with_body_array(self):
+        headers = {"id": 123, "action": "get"}
+        body = ["testing", "testing2"]
+        data_return_bytes = MinosAvroProtocol.encode(headers, body)
+        self.assertEqual(True, isinstance(data_return_bytes, bytes))
 
+        # decode
+        returned_dict = MinosAvroProtocol.decode(data_return_bytes)
+        self.assertEqual("get", returned_dict["headers"]["action"])
+        self.assertEqual(123, returned_dict["headers"]["id"])
+        self.assertEqual("testing", returned_dict["body"][0])
+        self.assertEqual("testing2", returned_dict["body"][1])
 
-def test_encoder_decoder_with_body_array():
-    headers = {"id": 123, "action": "get"}
-    body = ["testing", "testing2"]
-    data_return_bytes = MinosAvroProtocol.encode(headers, body)
-    assert isinstance(data_return_bytes, bytes) == True
+    def test_encoder_decoder_without_body(self):
+        headers = {"id": 123, "action": "get"}
+        data_return_bytes = MinosAvroProtocol.encode(headers)
+        self.assertEqual(True, isinstance(data_return_bytes, bytes))
 
-    # decode
-    returned_dict = MinosAvroProtocol.decode(data_return_bytes)
-    assert returned_dict["headers"]["action"] == "get"
-    assert returned_dict["headers"]["id"] == 123
-    assert returned_dict["body"][0] == "testing"
-    assert returned_dict["body"][1] == "testing2"
-
-
-def test_encoder_decoder_without_body():
-    headers = {"id": 123, "action": "get"}
-    data_return_bytes = MinosAvroProtocol.encode(headers)
-    assert isinstance(data_return_bytes, bytes) == True
-
-    # decode
-    returned_dict = MinosAvroProtocol.decode(data_return_bytes)
-    assert returned_dict["headers"]["action"] == "get"
-    assert returned_dict["headers"]["id"] == 123
+        # decode
+        returned_dict = MinosAvroProtocol.decode(data_return_bytes)
+        self.assertEqual("get", returned_dict["headers"]["action"])
+        self.assertEqual(123, returned_dict["headers"]["id"])
 
 
-def test_encoder_decoder_database_values_string():
-    value = "String Test"
-    data_return_bytes = MinosAvroValuesDatabase.encode(value)
-    assert isinstance(data_return_bytes, bytes) == True
+class TestMinosAvroValuesDatabase(unittest.TestCase):
 
-    # decode
-    returned_string = MinosAvroValuesDatabase.decode(data_return_bytes)
-    assert returned_string == "String Test"
+    def test_encoder_decoder_database_values_string(self):
+        value = "String Test"
+        data_return_bytes = MinosAvroValuesDatabase.encode(value)
+        self.assertEqual(True, isinstance(data_return_bytes, bytes))
+
+        # decode
+        returned_string = MinosAvroValuesDatabase.decode(data_return_bytes)
+        self.assertEqual("String Test", returned_string)
+
+    def test_encoder_decoder_database_values_dictionary(self):
+        value = {"first": "this is a string", "second": 123}
+        data_return_bytes = MinosAvroValuesDatabase.encode(value)
+        self.assertEqual(True, isinstance(data_return_bytes, bytes))
+
+        # decode
+        returned_string = MinosAvroValuesDatabase.decode(data_return_bytes)
+        self.assertEqual("this is a string", returned_string["first"])
+        self.assertEqual(123, returned_string["second"])
+
+    def test_encoder_decoder_database_values_list(self):
+        value = ["first", "second"]
+        data_return_bytes = MinosAvroValuesDatabase.encode(value)
+        self.assertEqual(True, isinstance(data_return_bytes, bytes))
+
+        # decode
+        returned_string = MinosAvroValuesDatabase.decode(data_return_bytes)
+        self.assertEqual("first", returned_string[0])
+        self.assertEqual("second", returned_string[1])
 
 
-def test_encoder_decoder_database_values_dictionary():
-    value = {"first": "this is a string", "second": 123}
-    data_return_bytes = MinosAvroValuesDatabase.encode(value)
-    assert isinstance(data_return_bytes, bytes) == True
-
-    # decode
-    returned_string = MinosAvroValuesDatabase.decode(data_return_bytes)
-    assert returned_string["first"] == "this is a string"
-    assert returned_string["second"] == 123
-
-
-def test_encoder_decoder_database_values_list():
-    value = ["first", "second"]
-    data_return_bytes = MinosAvroValuesDatabase.encode(value)
-    assert isinstance(data_return_bytes, bytes) == True
-
-    # decode
-    returned_string = MinosAvroValuesDatabase.decode(data_return_bytes)
-    assert returned_string[0] == "first"
-    assert returned_string[1] == "second"
+if __name__ == "__main__":
+    unittest.main()
