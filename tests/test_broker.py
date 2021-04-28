ce8e49df293808327ce7c2614c1da646398e003c
Sergio García Prado <sergio.garcia@clariteia.com>
2021-04-28 14:26:50 +0200

    ISSUE #143 * Extend event logic to be able to dynamically choose the proper object type.

--- tests/test_broker.py
+++ tests/test_broker.py
@@ -1,19 +1,54 @@
-import pytest
+"""
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+"""
+import unittest
+from typing import (
+    NoReturn,
+)
+
 from minos.common import (
-    MinosBaseBroker,
+    Aggregate,
+    Event,
+    MinosBaseBroker, MultiTypeMinosModelSequenceException,
+)
+from tests.aggregate_classes import (
+    Car, Owner,
 )
 
 
 class MinosBroker(MinosBaseBroker):
-    def __init__(self):
-        self.database = self._database()
-
-    def _database(self):
-        pass
-
-    def send(self):
+    async def send(self, items: list[Aggregate]) -> NoReturn:
         pass
 
 
-def test_minos_base_broker():
-    assert MinosBroker().send() is None
+class TestMinosBaseBroker(unittest.IsolatedAsyncioTestCase):
+    def test_topic(self):
+        broker = MinosBroker("CarAdded")
+        self.assertEqual("CarAdded", broker.topic)
+
+    async def test_send(self):
+        broker = MinosBroker("CarAdded")
+        self.assertEqual(None, await broker.send([Car(1, 1, 3, "red"), Car(1, 1, 3, "red")]))
+
+    async def test_send_one(self):
+        broker = MinosBroker("CarAdded")
+        self.assertEqual(None, await broker.send_one(Car(1, 1, 3, "red")))
+
+
+class TestEvent(unittest.TestCase):
+    def test_avro_serialization(self):
+        event = Event("CarCreated", [Car(1, 1, 3, "blue"), Car(2, 1, 5, "red")])
+        decoded_event = Event.from_avro_bytes(event.avro_bytes)
+        self.assertEqual(event, decoded_event)
+
+    def test_items_raises(self):
+        with self.assertRaises(MultiTypeMinosModelSequenceException):
+            Event("CarCreated", [Car(1, 1, 3, "blue"), Owner(2, 1, "Foo", "Bar")])
+
+
+if __name__ == "__main__":
+    unittest.main()
