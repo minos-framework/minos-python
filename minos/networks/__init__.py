e3a4c280a82e399df48fcf23af649ad2f465073d
Sergio García Prado <sergio.garcia@clariteia.com>
2021-04-29 08:11:32 +0200

    REFS #66 * Add docstring.

--- minos/networks/__init__.py
+++ minos/networks/__init__.py
@@ -1,3 +1,10 @@
+"""
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+"""
 __version__ = "0.0.1.1-alpha"
 
 from .broker import (
