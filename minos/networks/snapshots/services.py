7fbb35b9fae73b336aaeac547a2c355f17192cfd
Restyled.io <commits@restyled.io>
2021-04-29 07:49:19 +0000

    Restyled by isort

--- minos/networks/snapshots/services.py
+++ minos/networks/snapshots/services.py
@@ -5,12 +5,19 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from typing import Any
+from typing import (
+    Any,
+)
 
-from aiomisc.service.periodic import PeriodicService
-from minos.common import MinosConfig
-
-from minos.networks.snapshots import MinosSnapshotDispatcher
+from aiomisc.service.periodic import (
+    PeriodicService,
+)
+from minos.common import (
+    MinosConfig,
+)
+from minos.networks.snapshots import (
+    MinosSnapshotDispatcher,
+)
 
 
 class MinosSnapshotService(PeriodicService):
