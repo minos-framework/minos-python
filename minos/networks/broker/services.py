34d09c04c30f6b655b47c548bf573e8e31a59d16
Restyled.io <commits@restyled.io>
2021-04-29 13:00:54 +0000

    Restyled by isort

--- minos/networks/broker/services.py
+++ minos/networks/broker/services.py
@@ -5,12 +5,20 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from __future__ import annotations
+from __future__ import (
+    annotations,
+)
 
-from aiomisc.service.periodic import PeriodicService
-from minos.common import MinosConfig
+from aiomisc.service.periodic import (
+    PeriodicService,
+)
+from minos.common import (
+    MinosConfig,
+)
 
-from .dispatchers import MinosQueueDispatcher
+from .dispatchers import (
+    MinosQueueDispatcher,
+)
 
 
 class MinosQueueService(PeriodicService):
