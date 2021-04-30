400562a77116e359276da295959d14c383a5d684
Sergio García Prado <sergio.garcia@clariteia.com>
2021-04-30 12:36:58 +0200

    ISSUE #150 * Minor change (9).

--- minos/common/model/aggregate.py
+++ minos/common/model/aggregate.py
@@ -130,7 +130,7 @@ class Aggregate(MinosModel):
         if _repository is None:
             _repository = cls._build_repository(_config)
 
-        instance = cls(0, 0, *args, _broker=_broker, **kwargs)
+        instance = cls(0, 0, *args, _broker=_broker, _repository=_repository, **kwargs)
 
         entry = await _repository.insert(instance)
 
