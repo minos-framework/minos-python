43bb97d25494cd555aef24178f0e3de5a2c49655
Restyled.io <commits@restyled.io>
2021-04-30 15:14:42 +0000

    Restyled by black

--- minos/networks/snapshots/dispatchers.py
+++ minos/networks/snapshots/dispatchers.py
@@ -5,9 +5,7 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from __future__ import (
-    annotations,
-)
+from __future__ import annotations
 
 from typing import (
     Any,
@@ -29,12 +27,8 @@ from minos.common import (
     import_module,
 )
 
-from ..exceptions import (
-    MinosPreviousVersionSnapshotException,
-)
-from .entries import (
-    MinosSnapshotEntry,
-)
+from ..exceptions import MinosPreviousVersionSnapshotException
+from .entries import MinosSnapshotEntry
 
 
 class MinosSnapshotDispatcher(MinosSetup):
