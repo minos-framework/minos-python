4fd6daf8519a7c9eebbc3b10574e1994a24d21b6
Restyled.io <commits@restyled.io>
2021-05-03 11:24:18 +0000

    Restyled by black

--- minos/networks/broker/dispatchers.py
+++ minos/networks/broker/dispatchers.py
@@ -5,27 +5,22 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from __future__ import (
-    annotations,
-)
+from __future__ import annotations
 
 from typing import (
     NamedTuple,
     NoReturn,
-    Optional, AsyncIterator,
+    Optional,
+    AsyncIterator,
 )
 
-from aiokafka import (
-    AIOKafkaProducer,
-)
+from aiokafka import AIOKafkaProducer
 from minos.common import (
     MinosConfig,
     MinosConfigException,
 )
 
-from .abc import (
-    MinosBrokerSetup,
-)
+from .abc import MinosBrokerSetup
 
 
 class MinosQueueDispatcher(MinosBrokerSetup):
