0310235858b8f5e442dacd62ce23ca6ffd6151d0
Sergio García Prado <sergio.garcia@clariteia.com>
2021-05-05 13:37:47 +0200

    ISSUE #34 * Discard non needed change.

--- docs/conf.py
+++ docs/conf.py
@@ -19,11 +19,9 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 import minos
-
-sys.path.insert(0, os.path.abspath(".."))
-
 
 # -- General configuration ---------------------------------------------
 
@@ -33,22 +31,22 @@ sys.path.insert(0, os.path.abspath(".."))
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = ".rst"
+source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = "index"
+master_doc = 'index'
 
 # General information about the project.
-project = "Minos Microservice SAGA"
+project = 'Minos Microservice SAGA'
 copyright = "2021, Vladyslav Fenchak"
 author = "Vladyslav Fenchak"
 
@@ -71,10 +69,10 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
+pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
@@ -85,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
@@ -96,13 +94,13 @@ html_theme = "alabaster"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ---------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = "minos_microservice_sagadoc"
+htmlhelp_basename = 'minos_microservice_sagadoc'
 
 
 # -- Options for LaTeX output ------------------------------------------
@@ -111,12 +109,15 @@ latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
     # 'papersize': 'letterpaper',
+
     # The font size ('10pt', '11pt' or '12pt').
     #
     # 'pointsize': '10pt',
+
     # Additional stuff for the LaTeX preamble.
     #
     # 'preamble': '',
+
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -126,7 +127,9 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, "minos_microservice_saga.tex", "Minos Microservice SAGA Documentation", "Vladyslav Fenchak", "manual"),
+    (master_doc, 'minos_microservice_saga.tex',
+     'Minos Microservice SAGA Documentation',
+     'Vladyslav Fenchak', 'manual'),
 ]
 
 
@@ -134,7 +137,11 @@ latex_documents = [
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, "minos_microservice_saga", "Minos Microservice SAGA Documentation", [author], 1)]
+man_pages = [
+    (master_doc, 'minos_microservice_saga',
+     'Minos Microservice SAGA Documentation',
+     [author], 1)
+]
 
 
 # -- Options for Texinfo output ----------------------------------------
@@ -143,13 +150,13 @@ man_pages = [(master_doc, "minos_microservice_saga", "Minos Microservice SAGA Do
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (
-        master_doc,
-        "minos_microservice_saga",
-        "Minos Microservice SAGA Documentation",
-        author,
-        "minos_microservice_saga",
-        "One line description of project.",
-        "Miscellaneous",
-    ),
+    (master_doc, 'minos_microservice_saga',
+     'Minos Microservice SAGA Documentation',
+     author,
+     'minos_microservice_saga',
+     'One line description of project.',
+     'Miscellaneous'),
 ]
+
+
+
