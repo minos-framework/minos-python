988bdc9a779983059ddf6c331b78af042b5ac21d
Sergio García Prado <sergio.garcia@clariteia.com>
2021-05-11 13:07:21 +0200

    ISSUE #198 * Add docstring and minor changes.

--- minos/common/protocol/abc.py
+++ minos/common/protocol/abc.py
@@ -7,16 +7,31 @@ Minos framework can not be copied and/or distributed without the express permiss
 """
 
 import abc
-import typing as t
+from typing import Any
 
 
 class MinosBinaryProtocol(abc.ABC):
+    """Minos binary encoder / decoder interface."""
+
     @classmethod
     @abc.abstractmethod
-    def encode(cls, headers: t.Dict, body: t.Any) -> bytes:
+    def encode(cls, *args, **kwargs) -> bytes:
+        """Encodes the given value into bytes.
+
+        :param args: Additional positional arguments.
+        :param kwargs: Additional named arguments.
+        :return: A bytes instance.
+        """
         raise NotImplementedError
 
     @classmethod
     @abc.abstractmethod
-    def decode(cls, data: bytes):
+    def decode(cls, data: bytes, *args, **kwargs) -> Any:
+        """Decodes the given bytes data.
+
+        :param data: bytes data to be decoded.
+        :param args: Additional positional arguments.
+        :param kwargs: Additional named arguments.
+        :return: De decoded data.
+        """
         raise NotImplementedError
