e841ad18d79eab6c1c3d65df6f53fec4d254dc99
Sergio García Prado <sergio.garcia@clariteia.com>
2021-05-11 13:02:42 +0200

    ISSUE #198 * Rename module from `abstract` to `abc`.

--- minos/common/protocol/__init__.py
+++ minos/common/protocol/__init__.py
@@ -6,7 +6,7 @@ This file is part of minos framework.
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
 
-from .abstract import (
+from .abc import (
     MinosBinaryProtocol,
 )
 from .avro import (
