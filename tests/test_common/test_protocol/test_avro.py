9ff4bdde99136cce0c97fc614f239e3c9225e65f
Sergio García Prado <sergio.garcia@clariteia.com>
2021-05-12 11:38:17 +0200

    ISSUE #205 * Increase coverage.

--- tests/test_common/test_protocol/test_avro.py
+++ tests/test_common/test_protocol/test_avro.py
@@ -15,6 +15,10 @@ from minos.common import (
 
 
 class TestMinosAvroProtocol(unittest.TestCase):
+    def test_decode_avro_raises(self):
+        with self.assertRaises(MinosProtocolException):
+            MinosAvroProtocol.decode(bytes())
+
     def test_encoder_decoder_with_body_dict(self):
         headers = {"id": 123, "action": "get"}
         body = {"message": "this is a message"}
