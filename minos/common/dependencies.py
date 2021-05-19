18e3039614ee71440a24b47e157ca8850a91f261
Sergio García Prado <sergio.garcia@clariteia.com>
2021-05-19 17:13:03 +0200

    ISSUE #161 * Minor improvements related with the `minos.common.MinosDependencyContainer`.

--- minos/common/dependencies.py
+++ minos/common/dependencies.py
@@ -11,8 +11,9 @@ from __future__ import (
 )
 
 from typing import (
-    Any,
+    NoReturn,
     Optional,
+    Type,
 )
 
 from cached_property import (
@@ -38,29 +39,17 @@ from .saga import (
 
 
 class MinosDependencyContainer(object):
-    """TODO"""
+    """Wrapper class for ``dependency_injector.containers.Container``. """
 
     def __init__(
         self,
-        modules: list[Any],
         config: MinosConfig,
-        repository_cls: Optional[type[MinosRepository]] = None,
-        event_broker_cls: Optional[type[MinosBroker]] = None,
-        command_broker_cls: Optional[type[MinosBroker]] = None,
-        command_reply_broker_cls: Optional[type[MinosBroker]] = None,
-        saga_manager_cls: Optional[type[MinosSagaManager]] = None,
+        repository_cls: Optional[Type[MinosRepository]] = None,
+        event_broker_cls: Optional[Type[MinosBroker]] = None,
+        command_broker_cls: Optional[Type[MinosBroker]] = None,
+        command_reply_broker_cls: Optional[Type[MinosBroker]] = None,
+        saga_manager_cls: Optional[Type[MinosSagaManager]] = None,
     ):
-        """TODO
-        :param modules: TODO
-        :param config: TODO
-        :param repository_cls: TODO
-        :param event_broker_cls: TODO
-        :param command_broker_cls: TODO
-        :param command_reply_broker_cls: TODO
-        :param saga_manager_cls: TODO
-        :return: TODO
-        """
-        self.modules = modules
         self.config = config
         self.repository_cls = repository_cls
         self.event_broker_cls = event_broker_cls
@@ -69,16 +58,16 @@ class MinosDependencyContainer(object):
         self.saga_manager_cls = saga_manager_cls
 
     @cached_property
-    def container(self):
-        """TODO
-        :return: TODO
+    def container(self) -> containers.Container:
+        """Get the dependencies container.
+
+        :return: A ``Container`` instance.
         """
         container = containers.DynamicContainer()
-
         container.config = providers.Object(self.config)
 
         if self.repository_cls is not None:
-            container.repository = providers.Singleton(self.repository_cls.from_config, config=self.config)
+            container.repository = providers.Singleton(self.repository_cls.from_config, config=container.config)
         if self.event_broker_cls is not None:
             container.event_broker = providers.Singleton(self.event_broker_cls)
         if self.command_broker_cls is not None:
@@ -86,23 +75,23 @@ class MinosDependencyContainer(object):
         if self.command_reply_broker_cls is not None:
             container.command_reply_broker = providers.Singleton(self.command_reply_broker_cls)
         if self.saga_manager_cls is not None:
-            container.saga_manager = providers.Singleton(self.saga_manager_cls.from_config, config=self.config)
+            container.saga_manager = providers.Singleton(self.saga_manager_cls.from_config, config=container.config)
 
         return container
 
-    async def wire(self):
-        """TODO
+    async def wire(self, *args, **kwargs) -> NoReturn:
+        """Connect the configuration.
 
-        :return: TODO
+        :return: This method does not return anything.
         """
-        self.container.wire(modules=self.modules)
+        self.container.wire(*args, **kwargs)
         if self.repository_cls is not None:
             await self.container.repository().setup()
 
-    async def unwire(self) -> None:
-        """TODO
+    async def unwire(self) -> NoReturn:
+        """Disconnect the configuration.
 
-        :return: TODO
+        :return: This method does not return anything.
         """
         self.container.unwire()
         if self.repository_cls is not None:
--- tests/test_common/test_model/test_aggregate.py
+++ tests/test_common/test_model/test_aggregate.py
@@ -5,6 +5,7 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
+import sys
 import unittest
 
 from minos.common import (
@@ -134,12 +135,8 @@ class TestAggregateWithConfig(PostgresAsyncTestCase):
 
     async def asyncSetUp(self):
         await super().asyncSetUp()
-        import sys
-
-        self.container = MinosDependencyContainer(
-            [sys.modules[__name__]], self.config, repository_cls=PostgreSqlMinosRepository
-        )
-        await self.container.wire()
+        self.container = MinosDependencyContainer(self.config, repository_cls=PostgreSqlMinosRepository)
+        await self.container.wire(modules=[sys.modules[__name__]])
 
     async def asyncTearDown(self):
         await self.container.unwire()
