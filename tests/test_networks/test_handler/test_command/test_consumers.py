1205b7488050033b5610d5bf06435b673aab20eb
Sergio García Prado <sergio.garcia@clariteia.com>
2021-05-21 11:43:00 +0200

    ISSUE #110 #145 #146 * Fix problems related with non closed connection.

--- tests/test_networks/test_handler/test_command/test_consumers.py
+++ tests/test_networks/test_handler/test_command/test_consumers.py
@@ -42,43 +42,38 @@ class TestCommandServer(PostgresAsyncTestCase):
         bin_data = event_instance.avro_bytes
         Command.from_avro_bytes(bin_data)
 
-        event_server = CommandConsumer.from_config(config=self.config)
-        await event_server.setup()
-
-        id = await event_server.queue_add(topic=event_instance.topic, partition=0, binary=bin_data)
-        assert id > 0
+        async with CommandConsumer.from_config(config=self.config) as handler:
+            id = await handler.queue_add(topic=event_instance.topic, partition=0, binary=bin_data)
+            assert id > 0
 
     async def test_handle_message(self):
-        event_server = CommandConsumer.from_config(config=self.config)
-        await event_server.setup()
+        async with CommandConsumer.from_config(config=self.config) as handler:
+            model = NaiveAggregate(test_id=1, test=2, id=1, version=1)
+            event_instance = Command(
+                topic="AddOrder",
+                model=model.classname,
+                items=[],
+                saga_id="43434jhij",
+                task_id="juhjh34",
+                reply_on="mkk2334",
+            )
+            bin_data = event_instance.avro_bytes
 
-        model = NaiveAggregate(test_id=1, test=2, id=1, version=1)
-        event_instance = Command(
-            topic="AddOrder",
-            model=model.classname,
-            items=[],
-            saga_id="43434jhij",
-            task_id="juhjh34",
-            reply_on="mkk2334",
-        )
-        bin_data = event_instance.avro_bytes
+            Mensaje = namedtuple("Mensaje", ["topic", "partition", "value"])
 
-        Mensaje = namedtuple("Mensaje", ["topic", "partition", "value"])
+            async def consumer():
+                yield Mensaje(topic="TicketAdded", partition=0, value=bin_data)
 
-        async def consumer():
-            yield Mensaje(topic="TicketAdded", partition=0, value=bin_data)
-
-        await event_server.handle_message(consumer())
+            await handler.handle_message(consumer())
 
     async def test_handle_message_ko(self):
-        event_server = CommandConsumer.from_config(config=self.config)
-        await event_server.setup()
+        async with CommandConsumer.from_config(config=self.config) as handler:
 
-        bin_data = bytes(b"test")
+            bin_data = bytes(b"test")
 
-        Mensaje = namedtuple("Mensaje", ["topic", "partition", "value"])
+            Mensaje = namedtuple("Mensaje", ["topic", "partition", "value"])
 
-        async def consumer():
-            yield Mensaje(topic="TicketAdded", partition=0, value=bin_data)
+            async def consumer():
+                yield Mensaje(topic="TicketAdded", partition=0, value=bin_data)
 
-        await event_server.handle_message(consumer())
+            await handler.handle_message(consumer())
