3bbaa626f33f31f9be96406854dedfdcb26d8051
Sergio García Prado <sergio.garcia@clariteia.com>
2021-05-27 17:05:48 +0200

    ISSUE #274 * Move schemas to an independent file.

---
+++ minos/common/protocol/avro/__init__.py
@@ -0,0 +1,15 @@
+"""
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+"""
+from .protocols import (
+    MinosAvroProtocol,
+    MinosAvroValuesDatabase,
+)
+from .schemas import (
+    DATABASE_AVRO_SCHEMA,
+    MESSAGE_AVRO_SCHEMA,
+)
