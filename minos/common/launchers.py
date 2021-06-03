bd831f3a96597e61f48a20be8ae3fe4bc4b3dbce
Sergio García Prado <sergio.garcia@clariteia.com>
2021-06-03 17:04:40 +0200

    ISSUE #299 * Migrate `EntrypointLauncher`, `DependencyInjector` and `Service` from microservice.

---
+++ minos/common/launchers.py
@@ -0,0 +1,122 @@
+"""
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+"""
+from __future__ import (
+    annotations,
+)
+
+import logging
+from typing import (
+    NoReturn,
+)
+
+from aiomisc import (
+    Service,
+    entrypoint,
+    receiver,
+)
+from aiomisc.entrypoint import (
+    Entrypoint,
+)
+from cached_property import (
+    cached_property,
+)
+
+from .configuration import (
+    MinosConfig,
+)
+from .injectors import (
+    DependencyInjector,
+)
+from .setup import (
+    MinosSetup,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class EntrypointLauncher(MinosSetup):
+    """EntryPoint Launcher class."""
+
+    def __init__(self, services: list[Service], injector: DependencyInjector, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.services = services
+        self.injector = injector
+
+    @classmethod
+    def _from_config(cls, *args, config: MinosConfig, **kwargs) -> EntrypointLauncher:
+        return cls(*args, config=config, **kwargs)
+
+    def launch(self) -> NoReturn:
+        """Launch a new execution and keeps running forever..
+
+        :return: This method does not return anything.
+        """
+        logger.info("Starting microservice...")
+        with self.entrypoint as loop:  # pragma: no cover
+            logger.info("Microservice is up and running!")
+            loop.run_forever()
+
+    @cached_property
+    def entrypoint(self) -> Entrypoint:
+        """Entrypoint instance.
+
+        :return: An ``Entrypoint`` instance.
+        """
+
+        # noinspection PyUnusedLocal
+        @receiver(entrypoint.PRE_START)
+        async def _start(*args, **kwargs):
+            await self.setup()
+
+        # noinspection PyUnusedLocal
+        @receiver(entrypoint.POST_STOP)
+        async def _stop(*args, **kwargs):
+            await self.destroy()
+
+        return entrypoint(*self.services)
+
+    async def _setup(self) -> NoReturn:
+        """Wire the dependencies and setup it.
+
+        :return: This method does not return anything.
+        """
+        modules = list()
+        try:
+            from minos import (
+                common,
+            )
+
+            modules += [common]
+        except ImportError:
+            pass
+        try:
+            from minos import (
+                networks,
+            )
+
+            modules += [networks]
+        except ImportError:
+            pass
+
+        try:
+            from minos import (
+                saga,
+            )
+
+            modules += [saga]
+        except ImportError:
+            pass
+        await self.injector.wire(modules=modules)
+
+    async def _destroy(self) -> NoReturn:
+        """Unwire the injected dependencies and destroys it.
+
+        :return: This method does not return anything.
+        """
+        await self.injector.unwire()
