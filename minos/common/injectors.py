88a2d8e0d5db92ceb0825f9a9e4c6069bca9180f
Sergio García Prado <sergio.garcia@clariteia.com>
2021-06-04 08:49:23 +0200

    ISSUE #299 * Refactor `DependencyInjector` class.

--- minos/common/injectors.py
+++ minos/common/injectors.py
@@ -38,8 +38,8 @@ from .saga import (
 )
 
 
-class DependencyInjector(object):
-    """Wrapper class for ``dependency_injector.containers.Container``. """
+class DependencyInjector:
+    """Async wrapper of ``dependency_injector.containers.Container``. """
 
     def __init__(
         self,
@@ -57,36 +57,6 @@ class DependencyInjector(object):
         self.command_reply_broker_cls = command_reply_broker_cls
         self.saga_manager_cls = saga_manager_cls
 
-    @cached_property
-    def container(self) -> containers.Container:
-        """Get the dependencies container.
-
-        :return: A ``Container`` instance.
-        """
-        container = containers.DynamicContainer()
-        container.config = providers.Object(self.config)
-
-        if self.repository_cls is not None:
-            repository = self.repository_cls.from_config(config=self.config)
-            container.repository = providers.Object(repository)
-
-        if self.event_broker_cls is not None:
-            event_broker = self.event_broker_cls.from_config(config=self.config)
-            container.event_broker = providers.Object(event_broker)
-
-        if self.command_broker_cls is not None:
-            command_broker = self.command_broker_cls.from_config(config=self.config)
-            container.command_broker = providers.Object(command_broker)
-
-        if self.command_reply_broker_cls is not None:
-            command_reply_broker = self.command_reply_broker_cls.from_config(config=self.config)
-            container.command_reply_broker = providers.Object(command_reply_broker)
-
-        if self.saga_manager_cls is not None:
-            container.saga_manager = providers.Object(self.saga_manager_cls.from_config(config=self.config))
-
-        return container
-
     async def wire(self, *args, **kwargs) -> NoReturn:
         """Connect the configuration.
 
@@ -94,20 +64,20 @@ class DependencyInjector(object):
         """
         self.container.wire(*args, **kwargs)
 
-        if self.repository_cls is not None:
-            await self.container.repository().setup()
+        if self.repository is not None:
+            await self.repository.setup()
 
-        if self.event_broker_cls is not None:
-            await self.container.event_broker().setup()
+        if self.event_broker is not None:
+            await self.event_broker.setup()
 
-        if self.command_broker_cls is not None:
-            await self.container.command_broker().setup()
+        if self.command_broker is not None:
+            await self.command_broker.setup()
 
-        if self.command_reply_broker_cls is not None:
-            await self.container.command_reply_broker().setup()
+        if self.command_reply_broker is not None:
+            await self.command_reply_broker.setup()
 
-        if self.saga_manager_cls is not None:
-            await self.container.saga_manager().setup()
+        if self.saga_manager is not None:
+            await self.saga_manager.setup()
 
     async def unwire(self) -> NoReturn:
         """Disconnect the configuration.
@@ -116,20 +86,82 @@ class DependencyInjector(object):
         """
         self.container.unwire()
 
-        if self.repository_cls is not None:
-            await self.container.repository().destroy()
+        if self.repository is not None:
+            await self.repository.destroy()
 
-        if self.repository_cls is not None:
-            await self.container.repository().destroy()
+        if self.event_broker is not None:
+            await self.event_broker.destroy()
 
-        if self.event_broker_cls is not None:
-            await self.container.event_broker().destroy()
+        if self.command_broker is not None:
+            await self.command_broker.destroy()
 
-        if self.command_broker_cls is not None:
-            await self.container.command_broker().destroy()
-
-        if self.command_reply_broker_cls is not None:
-            await self.container.command_reply_broker().destroy()
+        if self.command_reply_broker is not None:
+            await self.command_reply_broker.destroy()
 
         if self.saga_manager_cls is not None:
-            await self.container.saga_manager().destroy()
+            await self.saga_manager.destroy()
+
+    @cached_property
+    def container(self) -> containers.Container:
+        """Get the dependencies container.
+
+        :return: A ``Container`` instance.
+        """
+        container = containers.DynamicContainer()
+        container.config = providers.Object(self.config)
+        container.repository = providers.Object(self.repository)
+        container.event_broker = providers.Object(self.event_broker)
+        container.command_broker = providers.Object(self.command_broker)
+        container.command_reply_broker = providers.Object(self.command_reply_broker)
+        container.saga_manager = providers.Object(self.saga_manager)
+        return container
+
+    @cached_property
+    def repository(self) -> Optional[MinosRepository]:
+        """Get the Repository instance to be injected.
+
+        :return: A `` MinosRepository`` instance or ``None``.
+        """
+        if self.repository_cls is None:
+            return None
+        return self.repository_cls.from_config(config=self.config)
+
+    @cached_property
+    def event_broker(self) -> Optional[MinosBroker]:
+        """Get the Event Broker instance to be injected.
+
+        :return: A `` MinosBroker`` instance or ``None``.
+        """
+        if self.event_broker_cls is None:
+            return None
+        return self.event_broker_cls.from_config(config=self.config)
+
+    @cached_property
+    def command_broker(self) -> Optional[MinosBroker]:
+        """Get the Command Broker instance to be injected.
+
+        :return: A `` MinosBroker`` instance or ``None``.
+        """
+        if self.command_broker_cls is None:
+            return None
+        return self.command_broker_cls.from_config(config=self.config)
+
+    @cached_property
+    def command_reply_broker(self) -> Optional[MinosBroker]:
+        """Get the Command Reply Broker instance to be injected.
+
+        :return: A `` MinosBroker`` instance or ``None``.
+        """
+        if self.command_reply_broker_cls is None:
+            return None
+        return self.command_reply_broker_cls.from_config(config=self.config)
+
+    @cached_property
+    def saga_manager(self) -> Optional[MinosSagaManager]:
+        """Get the Saga Manager instance to be injected.
+
+        :return: A `` MinosSagaManager`` instance or ``None``.
+        """
+        if self.saga_manager_cls is None:
+            return None
+        return self.saga_manager_cls.from_config(config=self.config)
--- tests/test_common/test_injectors.py
+++ tests/test_common/test_injectors.py
@@ -12,6 +12,10 @@ from unittest.mock import (
     call,
 )
 
+from dependency_injector.containers import (
+    Container,
+)
+
 from minos.common import (
     DependencyInjector,
     MinosConfig,
@@ -29,29 +33,50 @@ class TestMinosDependencyInjector(unittest.IsolatedAsyncioTestCase):
         self.config_file_path = BASE_PATH / "test_config.yml"
         self.config = MinosConfig(path=str(self.config_file_path))
 
+    def test_repository(self):
+        injector = DependencyInjector(self.config, repository_cls=FakeRepository)
+        self.assertIsInstance(injector.repository, FakeRepository)
+
+    def test_event_broker(self):
+        injector = DependencyInjector(self.config, event_broker_cls=FakeBroker)
+        self.assertIsInstance(injector.event_broker, FakeBroker)
+
+    def test_command_broker(self):
+        injector = DependencyInjector(self.config, command_broker_cls=FakeBroker)
+        self.assertIsInstance(injector.command_broker, FakeBroker)
+
+    def test_command_reply_broker(self):
+        injector = DependencyInjector(self.config, command_reply_broker_cls=FakeBroker)
+        self.assertIsInstance(injector.container.command_reply_broker(), FakeBroker)
+
+    def test_saga_manager(self):
+        injector = DependencyInjector(self.config, saga_manager_cls=FakeSagaManager)
+        self.assertIsInstance(injector.saga_manager, FakeSagaManager)
+
     def test_container(self):
         injector = DependencyInjector(self.config)
+        self.assertIsInstance(injector.container, Container)
         self.assertEqual(self.config, injector.container.config())
 
     def test_container_repository(self):
         injector = DependencyInjector(self.config, repository_cls=FakeRepository)
-        self.assertIsInstance(injector.container.repository(), FakeRepository)
+        self.assertEqual(injector.repository, injector.container.repository())
 
     def test_container_event_broker(self):
         injector = DependencyInjector(self.config, event_broker_cls=FakeBroker)
-        self.assertIsInstance(injector.container.event_broker(), FakeBroker)
+        self.assertEqual(injector.event_broker, injector.container.event_broker())
 
     def test_container_command_broker(self):
         injector = DependencyInjector(self.config, command_broker_cls=FakeBroker)
-        self.assertIsInstance(injector.container.command_broker(), FakeBroker)
+        self.assertEqual(injector.command_broker, injector.container.command_broker())
 
     def test_container_command_reply_broker(self):
         injector = DependencyInjector(self.config, command_reply_broker_cls=FakeBroker)
-        self.assertIsInstance(injector.container.command_reply_broker(), FakeBroker)
+        self.assertEqual(injector.command_reply_broker, injector.container.command_reply_broker())
 
     def test_container_saga_manager(self):
         injector = DependencyInjector(self.config, saga_manager_cls=FakeSagaManager)
-        self.assertIsInstance(injector.container.saga_manager(), FakeSagaManager)
+        self.assertEqual(injector.saga_manager, injector.container.saga_manager())
 
     async def test_wire_unwire(self):
         injector = DependencyInjector(
