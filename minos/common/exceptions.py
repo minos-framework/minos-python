a1c6aadc7d26b0d2bf9a0e6be07092d040ea4c6e
Sergio García Prado <sergio.garcia@clariteia.com>
2021-06-09 16:51:54 +0200

    ISSUE #315 * Remove duplicated code.

--- minos/common/exceptions.py
+++ minos/common/exceptions.py
@@ -101,6 +101,18 @@ class MinosSnapshotNotProvidedException(MinosSnapshotException):
     """Exception to be raised when a snapshot is needed but none is set."""
 
 
+class MinosPreviousVersionSnapshotException(MinosSnapshotException):
+    """Exception to be raised when current version is newer than the one to be processed."""
+
+    def __init__(self, previous: Aggregate, new: Aggregate):
+        self.previous = previous
+        self.new = new
+        super().__init__(
+            f"Version for {repr(previous.classname)} aggregate must be "
+            f"greater than {previous.version}. Obtained: {new.version}"
+        )
+
+
 class MinosModelException(MinosException):
     """Exception to be raised when some mandatory condition is not satisfied by a model."""
 
@@ -167,19 +179,3 @@ class MinosAttributeValidationException(MinosModelAttributeException):
         self.name = name
         self.value = value
         super().__init__(f"{repr(value)} value does not pass the {repr(name)} field validation.")
-
-
-class MinosSnapshotException(MinosException):
-    """Base snapshot exception"""
-
-
-class MinosPreviousVersionSnapshotException(MinosSnapshotException):
-    """Exception to be raised when current version is newer than the one to be processed."""
-
-    def __init__(self, previous: Aggregate, new: Aggregate):
-        self.previous = previous
-        self.new = new
-        super().__init__(
-            f"Version for {repr(previous.classname)} aggregate must be "
-            f"greater than {previous.version}. Obtained: {new.version}"
-        )
