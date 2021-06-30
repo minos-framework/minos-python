232a4b0b62362e35cf9898021c05f247bceab9f1
Sergio García Prado <sergio.garcia@clariteia.com>
2021-06-30 11:59:44 +0200

    ISSUE #358 * Define `__eq__` and `__repr__` on `minos.common.Request` and `minos.common.Response`. * Add tests.

--- minos/common/messages.py
+++ minos/common/messages.py
@@ -5,6 +5,10 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
+from __future__ import (
+    annotations,
+)
+
 from abc import (
     ABC,
     abstractmethod,
@@ -27,6 +31,15 @@ class Request(ABC):
 
         :return: A list of instances.
         """
+        raise NotImplementedError
+
+    @abstractmethod
+    def __eq__(self, other: Request) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __repr__(self) -> str:
+        raise NotImplementedError
 
 
 class Response:
@@ -52,3 +65,10 @@ class Response:
         :return: A list of raw items.
         """
         return [item if not isinstance(item, Model) else item.avro_data for item in self._items]
+
+    @abstractmethod
+    def __eq__(self, other: Response) -> bool:
+        return type(self) == type(other) and self._items == other._items
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({self._items!r})"
