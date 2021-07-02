75eb5b08a170135809e919f881d842cbb0a187c2
Sergio García Prado <sergio.garcia@clariteia.com>
2021-07-02 10:15:10 +0200

    ISSUE #377 * Add `minos.common.ResponseException`.

--- minos/common/messages.py
+++ minos/common/messages.py
@@ -76,3 +76,13 @@ class Response:
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}({self._items!r})"
+
+
+class ResponseException(Exception):
+    """TODO"""
+
+    def __init__(self, message: str):
+        self.message = message
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({self.message!r})"
