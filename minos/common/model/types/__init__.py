ffd2cc1c146d942c6f5c51c733db1e2317d81b89
Sergio García Prado <sergio.garcia@clariteia.com>
2021-07-13 11:29:36 +0200

    ISSUE #403 * Fix bug related with bad behaviour of `TypeHintBuilder` together with `ModelRef`.

--- minos/common/model/types/__init__.py
+++ minos/common/model/types/__init__.py
@@ -7,6 +7,7 @@ Minos framework can not be copied and/or distributed without the express permiss
 """
 from .builders import (
     TypeHintBuilder,
+    build_union,
 )
 from .comparators import (
     TypeHintComparator,
