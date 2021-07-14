3ad759a73f5a8d1bac83e9af24491339da879407
Sergio García Prado <sergio.garcia@clariteia.com>
2021-07-14 15:55:11 +0200

    ISSUE #414 * Minor change.

--- tests/test_common/test_model/test_types/test_model_refs.py
+++ tests/test_common/test_model/test_types/test_model_refs.py
@@ -31,39 +31,51 @@ class TestModelRefExtractor(unittest.TestCase):
     def test_simple(self):
         mt_foo = ModelType.build("Foo", {"uuid": UUID, "version": int})
         value = uuid4()
+
         expected = {"Foo": {value}}
         observed = ModelRefExtractor(value, ModelRef[mt_foo]).build()
+
         self.assertEqual(expected, observed)
 
     def test_list(self):
         mt_foo = ModelType.build("Foo", {"uuid": UUID, "version": int})
         value = [uuid4(), uuid4()]
+
         expected = {"Foo": set(value)}
         observed = ModelRefExtractor(value, list[ModelRef[mt_foo]]).build()
+
         self.assertEqual(expected, observed)
 
     def test_dict(self):
         mt_key = ModelType.build("Key", {"uuid": UUID, "version": int})
         mt_value = ModelType.build("Value", {"uuid": UUID, "version": int})
         value = {uuid4(): uuid4(), uuid4(): uuid4()}
+
         expected = {"Key": set(value.keys()), "Value": set(value.values())}
         observed = ModelRefExtractor(value, dict[ModelRef[mt_key], ModelRef[mt_value]]).build()
+
         self.assertEqual(expected, observed)
 
     def test_model(self):
         mt_bar = ModelType.build("Bar", {"uuid": UUID, "version": int})
         mt_foo = ModelType.build("Foo", {"uuid": UUID, "version": int, "another": ModelRef[mt_bar]})
+
         value = mt_foo(uuid=uuid4(), version=1, another=uuid4())
+
         expected = {"Bar": {value.another}}
         observed = ModelRefExtractor(value, mt_foo).build()
+
         self.assertEqual(expected, observed)
 
     def test_model_without_kind(self):
         mt_bar = ModelType.build("Bar", {"uuid": UUID, "version": int})
         mt_foo = ModelType.build("Foo", {"uuid": UUID, "version": int, "another": ModelRef[mt_bar]})
+
         value = mt_foo(uuid=uuid4(), version=1, another=uuid4())
+
         expected = ModelRefExtractor(value, mt_foo).build()
         observed = ModelRefExtractor(value).build()
+
         self.assertEqual(expected, observed)
 
 
