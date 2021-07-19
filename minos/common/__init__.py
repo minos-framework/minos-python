18781f114b12d9093dd6c0c6e9c091a02ccd9681
andrea-mucci <81492948+andrea-mucci@users.noreply.github.com>
2021-07-19 10:58:52 +0200

    v0.1.4

--- minos/common/__init__.py
+++ minos/common/__init__.py
@@ -6,7 +6,7 @@ This file is part of minos framework.
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 from .configuration import (
     BROKER,
