8eafb9b0914c2c82e1a54ba3465b2f963524c034
Sergio García Prado <sergio.garcia@clariteia.com>
2021-07-19 17:15:40 +0200

    ISSUE #132 * Remove warning.

--- minos/saga/definitions/types.py
+++ minos/saga/definitions/types.py
@@ -5,7 +5,7 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from collections import (
+from collections.abc import (
     Callable,
 )
 from typing import (
