e3dd34d72b4111005f79fda3b1cfc6b3be0e4dfb
Alberto Amigo Alonso <albamigh@gmail.com>
2021-08-03 12:06:06 +0200

    ISSUE #475
    - Changed test.

--- tests/test_common/test_constants.py
+++ tests/test_common/test_constants.py
@@ -5,8 +5,10 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-
 import unittest
+from uuid import (
+    uuid4,
+)
 
 from minos.common import (
     NULL_UUID,
@@ -19,7 +21,8 @@ class TestConstants(unittest.TestCase):
         self.assertEqual(r"00000000-0000-0000-0000-000000000000", str(NULL_UUID))
 
     def test_uuid_regex(self):
-        self.assertEqual(r"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}", UUID_REGEX.pattern)
+        uuid = str(uuid4())
+        self.assertRegex(uuid, UUID_REGEX)
 
 
 if __name__ == "__main__":
