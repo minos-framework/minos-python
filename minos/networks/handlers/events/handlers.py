21177a8a9ed36e3b9e9d9e782d92f36aba2ec1d0
Sergio García Prado <sergio.garcia@clariteia.com>
2021-08-06 13:07:39 +0200

    ISSUE #330 * Add initial implementation of event dispatching ensuring ordering.

--- minos/networks/handlers/events/handlers.py
+++ minos/networks/handlers/events/handlers.py
@@ -9,8 +9,17 @@ from __future__ import (
 )
 
 import logging
+from asyncio import (
+    gather,
+)
+from collections import (
+    defaultdict,
+)
 from inspect import (
     isawaitable,
+)
+from operator import (
+    attrgetter,
 )
 from typing import (
     Awaitable,
@@ -41,6 +50,9 @@ from ..messages import (
 
 logger = logging.getLogger(__name__)
 
+uuid_getter = attrgetter("data.data.uuid")
+version_getter = attrgetter("data.data.version")
+
 
 class EventHandler(Handler):
     """Event Handler class."""
@@ -55,6 +67,22 @@ class EventHandler(Handler):
         handlers = {decorator.topic: fn for decorator, fn in decorators.items()}
 
         return cls(handlers=handlers, **config.broker.queue._asdict(), **kwargs)
+
+    async def _dispatch_entries(self, entries: list[HandlerEntry[Event]]) -> NoReturn:
+        grouped = defaultdict(list)
+        for entry in entries:
+            uuid = uuid_getter(entry)
+            grouped[uuid].append(entry)
+
+        for group in grouped.values():
+            group.sort(key=version_getter)
+
+        futures = (self._dispatch_group(group) for group in grouped.values())
+        await gather(*futures)
+
+    async def _dispatch_group(self, entries: list[HandlerEntry[Event]]):
+        for entry in entries:
+            await self._dispatch_one(entry)
 
     async def dispatch_one(self, entry: HandlerEntry[Event]) -> NoReturn:
         """Dispatch one row.
