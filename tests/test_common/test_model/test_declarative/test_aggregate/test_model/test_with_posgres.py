c42312d30fc5a57e29d29674a0a3673311362f10
Sergio García Prado <sergio.garcia@clariteia.com>
2021-08-13 09:01:47 +0200

    ISSUE #515 * Refactor test.

--- tests/test_common/test_model/test_declarative/test_aggregate/test_model/test_with_posgres.py
+++ tests/test_common/test_model/test_declarative/test_aggregate/test_model/test_with_posgres.py
@@ -5,15 +5,10 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-import sys
 import unittest
 
-from dependency_injector import (
-    containers,
-    providers,
-)
-
 from minos.common import (
+    EntitySet,
     MinosSnapshotDeletedAggregateException,
     PostgreSqlRepository,
     PostgreSqlSnapshot,
@@ -23,6 +18,8 @@ from minos.common.testing import (
 )
 from tests.aggregate_classes import (
     Car,
+    Order,
+    OrderItem,
 )
 from tests.utils import (
     BASE_PATH,
@@ -30,52 +27,73 @@ from tests.utils import (
 )
 
 
-class TestAggregateWithPostgres(PostgresAsyncTestCase):
+class TestAggregateWithPostgreSql(PostgresAsyncTestCase):
     CONFIG_FILE_PATH = BASE_PATH / "test_config.yml"
 
     async def asyncSetUp(self):
         await super().asyncSetUp()
-        self.container = containers.DynamicContainer()
-        self.container.event_broker = providers.Object(FakeBroker())
-        self.container.repository = providers.Singleton(PostgreSqlRepository.from_config, config=self.config)
-        self.container.snapshot = providers.Singleton(
-            PostgreSqlSnapshot.from_config, config=self.config, repository=self.container.repository
-        )
-        await self.container.repository().setup()
-        await self.container.snapshot().setup()
-        self.container.wire(modules=[sys.modules[__name__]])
+        self.event_broker = FakeBroker()
+        self.repository = PostgreSqlRepository.from_config(config=self.config)
+        self.snapshot = PostgreSqlSnapshot.from_config(config=self.config, repository=self.repository)
 
-    async def asyncTearDown(self):
-        self.container.unwire()
-        await self.container.repository().destroy()
-        await self.container.snapshot().destroy()
-        await super().asyncTearDown()
+        self.kwargs = {
+            "_broker": self.event_broker,
+            "_repository": self.repository,
+            "_snapshot": self.snapshot,
+        }
 
     async def test_update(self):
-        car = await Car.create(doors=3, color="blue")
-        uuid = car.uuid
+        async with self.event_broker, self.repository, self.snapshot:
+            car = await Car.create(doors=3, color="blue", **self.kwargs)
+            uuid = car.uuid
 
-        await car.update(color="red")
-        self.assertEqual(Car(3, "red", uuid=uuid, version=2), car)
-        self.assertEqual(car, await Car.get_one(car.uuid))
+            await car.update(color="red")
+            self.assertEqual(Car(3, "red", uuid=uuid, version=2, **self.kwargs), car)
+            self.assertEqual(car, await Car.get_one(car.uuid, **self.kwargs))
 
-        await car.update(doors=5)
-        self.assertEqual(Car(5, "red", uuid=uuid, version=3), car)
-        self.assertEqual(car, await Car.get_one(car.uuid))
+            await car.update(doors=5)
+            self.assertEqual(Car(5, "red", uuid=uuid, version=3, **self.kwargs), car)
+            self.assertEqual(car, await Car.get_one(car.uuid, **self.kwargs))
 
-        await car.delete()
-        with self.assertRaises(MinosSnapshotDeletedAggregateException):
-            await Car.get_one(car.uuid)
+            await car.delete()
+            with self.assertRaises(MinosSnapshotDeletedAggregateException):
+                await Car.get_one(car.uuid, **self.kwargs)
 
-        car = await Car.create(doors=3, color="blue")
-        uuid = car.uuid
+            car = await Car.create(doors=3, color="blue", **self.kwargs)
+            uuid = car.uuid
 
-        await car.update(color="red")
-        self.assertEqual(Car(3, "red", uuid=uuid, version=2), await Car.get_one(car.uuid))
+            await car.update(color="red")
+            self.assertEqual(
+                Car(3, "red", uuid=uuid, version=2, **self.kwargs), await Car.get_one(car.uuid, **self.kwargs)
+            )
 
-        await car.delete()
-        with self.assertRaises(MinosSnapshotDeletedAggregateException):
-            await Car.get_one(car.uuid)
+            await car.delete()
+            with self.assertRaises(MinosSnapshotDeletedAggregateException):
+                await Car.get_one(car.uuid, **self.kwargs)
+
+    async def test_entity_set(self):
+        async with self.event_broker, self.repository, self.snapshot:
+            order = await Order.create(products=EntitySet({OrderItem(12)}), **self.kwargs)
+            item = OrderItem(24)
+            order.products.add(item)
+
+            await order.save()
+
+            recovered = await Order.get_one(order.uuid, **self.kwargs)
+            self.assertEqual(order, recovered)
+
+            item.amount = 36
+            order.products.add(item)
+            await order.save()
+
+            recovered = await Order.get_one(order.uuid, **self.kwargs)
+            self.assertEqual(order, recovered)
+
+            order.products.remove(item)
+            await order.save()
+
+            recovered = await Order.get_one(order.uuid, **self.kwargs)
+            self.assertEqual(order, recovered)
 
 
 if __name__ == "__main__":
