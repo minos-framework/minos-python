578b45a7e651e490acef5887c8d5053504bcce51
Sergio García Prado <sergio.garcia@clariteia.com>
2021-08-25 16:29:13 +0200

    ISSUE #343 * Refactor `DynamicReplyHandler` to consume messages from the database queue.

--- minos/networks/handlers/dynamic/handlers.py
+++ minos/networks/handlers/dynamic/handlers.py
@@ -24,18 +24,30 @@ from typing import (
     Union,
 )
 
+import aiopg
 from aiokafka import (
     AIOKafkaConsumer,
+)
+from psycopg2.sql import (
+    SQL,
+    Identifier,
 )
 
 from minos.common import (
     BROKER,
     MinosConfig,
     MinosHandler,
+    Model,
 )
 
 from ...exceptions import (
     MinosHandlerNotFoundEnoughEntriesException,
+)
+from ...utils import (
+    consume_queue,
+)
+from ..abc import (
+    Handler,
 )
 from ..entries import (
     HandlerEntry,
@@ -131,43 +143,45 @@ class DynamicHandler(MinosHandler):
         return 0, record.topic, record.partition, record.value, 0, datetime.now()
 
 
-class DynamicReplyHandler(MinosHandler):
+class DynamicReplyHandler(Handler):
     """Dynamic Reply Handler class."""
 
-    def __init__(self, topic, broker: Optional[BROKER] = None, **kwargs):
+    TABLE_NAME = "dynamic_queue"
+    ENTRY_MODEL_CLS = Model
+
+    async def dispatch_one(self, entry: HandlerEntry) -> NoReturn:
+        pass
+
+    def __init__(self, topic, **kwargs):
         super().__init__(**kwargs)
-        self._broker = broker
 
         self.topic = topic
-
         self._real_topic = topic if topic.endswith("Reply") else f"{topic}Reply"
-        self.consumer = AIOKafkaConsumer(self._real_topic, bootstrap_servers=f"{self.broker_host}:{self.broker_port}")
 
-    @property
-    def broker_host(self) -> str:
-        """Broker host getter.
+        self._connection = None
 
-        :return: A string value.
-        """
-        return self._broker.host
+    async def _setup(self) -> None:
+        await super()._setup()
 
-    @property
-    def broker_port(self) -> int:
-        """Broker port getter.
+        self._connection = await aiopg.connect(
+            database=self.database, user=self.user, password=self.password, host=self.host, port=self.port
+        )
+        async with self._connection.cursor() as cursor:
+            # noinspection PyTypeChecker
+            await cursor.execute(SQL("LISTEN {}").format(Identifier(self._real_topic)))
 
-        :return: An integer value.
-        """
-        return self._broker.port
+    async def _destroy(self) -> None:
+        if self._connection is not None:
+            async with self._connection.cursor() as cursor:
+                await cursor.execute(SQL("UNLISTEN {}").format(Identifier(self._real_topic)))
+            self._connection.close()
+            self._connection = None
+
+        await super()._destroy()
 
     @classmethod
     def _from_config(cls, *args, config: MinosConfig, **kwargs) -> DynamicReplyHandler:
-        return cls(broker=config.broker, **kwargs)
-
-    async def _setup(self) -> NoReturn:
-        await self.consumer.start()
-
-    async def _destroy(self) -> NoReturn:
-        await self.consumer.stop()
+        return cls(handlers=dict(), **config.broker.queue._asdict(), **kwargs)
 
     async def get_one(self, *args, **kwargs) -> HandlerEntry:
         """Get one handler entry from the given topics.
@@ -186,22 +200,30 @@ class DynamicReplyHandler(MinosHandler):
         :return: A list of ``HandlerEntry`` instances.
         """
         try:
-            raw = await wait_for(self._get_many(count), timeout=timeout)
+            entries = await wait_for(self._get_many(count), timeout=timeout)
         except TimeoutError:
             raise MinosHandlerNotFoundEnoughEntriesException(
                 f"Timeout exceeded while trying to fetch {count!r} entries from {self._real_topic!r}."
             )
 
-        entries = [HandlerEntry(0, record.topic, record.partition, record.value) for record in raw]
-
         logger.info(f"Dispatching '{entries if count > 1 else entries[0]!s}'...")
 
         return entries
 
-    async def _get_many(self, count: int) -> list[Any]:
+    async def _get_many(self, count: int) -> list[HandlerEntry]:
+        result = list()
+        async with self._connection.cursor() as cursor:
+            while len(result) < count:
+                await consume_queue(self._connection.notifies, count - len(result))
+                async with cursor.begin():
+                    await cursor.execute(_SELECT_NON_PROCESSED_ROWS_QUERY, (self._real_topic, count - len(result)))
+                    for entry in self._build_entries(await cursor.fetchall()):
+                        await cursor.execute(self._queries["delete_processed"], (entry.id,))
+                        result.append(entry)
 
-        raw = list()
-        while len(raw) < count:
-            raw.append(await self.consumer.getone())
+        return result
 
-        return raw
+
+_SELECT_NON_PROCESSED_ROWS_QUERY = SQL(
+    "SELECT * FROM dynamic_queue WHERE topic = %s ORDER BY creation_date LIMIT %s FOR UPDATE SKIP LOCKED"
+)
