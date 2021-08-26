0e52bdab7786ae830c91fe44b8bf13778e12a9e1
Sergio García Prado <sergio.garcia@clariteia.com>
2021-08-26 08:57:27 +0200

    ISSUE #343 * Update `DynamicReplyHandler` tests.

--- tests/test_networks/test_handlers/test_dynamic/test_handlers.py
+++ tests/test_networks/test_handlers/test_dynamic/test_handlers.py
@@ -1,28 +1,19 @@
-"""
-Copyright (C) 2021 Clariteia SL
+"""tests.test_networks.test_handlers.test_dynamic.test_handlers module."""
 
-This file is part of minos framework.
-
-Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
-"""
-import asyncio
 import unittest
 from datetime import (
     timedelta,
 )
-from unittest.mock import (
-    patch,
-)
 
-from minos.common import (
-    MinosHandler,
-)
+import aiopg
+
 from minos.common.testing import (
     PostgresAsyncTestCase,
 )
 from minos.networks import (
     DynamicReplyHandler,
     HandlerEntry,
+    HandlerSetup,
     MinosHandlerNotFoundEnoughEntriesException,
 )
 from tests.utils import (
@@ -50,32 +41,31 @@ class TestDynamicReplyHandler(PostgresAsyncTestCase):
         self.assertTrue(self.handler.already_destroyed)
 
     def test_base_classes(self):
-        self.assertIsInstance(self.handler, MinosHandler)
+        self.assertIsInstance(self.handler, HandlerSetup)
 
     async def test_get_one(self):
-        with patch("aiokafka.AIOKafkaConsumer.getone") as mock:
-            mock.side_effect = [
-                Message("foo", 0, FakeModel("test").avro_bytes),
-                Message("foo", 0, FakeModel("test").avro_bytes),
-            ]
-            expected = (await self.handler.get_many(count=1))[0]
+        expected = HandlerEntry(1, "fooReply", 0, FakeModel("test1").avro_bytes)
+        async with self.handler:
+            await self._insert_one(Message("foo", 0, FakeModel("test1").avro_bytes))
+            await self._insert_one(Message("foo", 0, FakeModel("test2").avro_bytes))
+
             observed = await self.handler.get_one()
-            self._assert_equal_entries(expected, observed)
+
+        self._assert_equal_entries(expected, observed)
 
     async def test_get_many(self):
         expected = [
-            HandlerEntry(0, "foo", 0, FakeModel("test1").avro_bytes),
-            HandlerEntry(0, "foo", 0, FakeModel("test2").avro_bytes),
-            HandlerEntry(0, "bar", 0, FakeModel("test3").avro_bytes),
-            HandlerEntry(0, "bar", 0, FakeModel("test4").avro_bytes),
+            HandlerEntry(1, "fooReply", 0, FakeModel("test1").avro_bytes),
+            HandlerEntry(2, "fooReply", 0, FakeModel("test2").avro_bytes),
+            HandlerEntry(3, "fooReply", 0, FakeModel("test3").avro_bytes),
+            HandlerEntry(4, "fooReply", 0, FakeModel("test4").avro_bytes),
         ]
-        with patch("aiokafka.AIOKafkaConsumer.getone") as mock:
-            mock.side_effect = [
-                Message("foo", 0, FakeModel("test1").avro_bytes),
-                Message("foo", 0, FakeModel("test2").avro_bytes),
-                Message("bar", 0, FakeModel("test3").avro_bytes),
-                Message("bar", 0, FakeModel("test4").avro_bytes),
-            ]
+        async with self.handler:
+            await self._insert_one(Message("foo", 0, FakeModel("test1").avro_bytes))
+            await self._insert_one(Message("foo", 0, FakeModel("test2").avro_bytes))
+            await self._insert_one(Message("foo", 0, FakeModel("test3").avro_bytes))
+            await self._insert_one(Message("foo", 0, FakeModel("test4").avro_bytes))
+
             observed = await self.handler.get_many(count=4)
 
         self.assertEqual(len(expected), len(observed))
@@ -83,10 +73,20 @@ class TestDynamicReplyHandler(PostgresAsyncTestCase):
             self._assert_equal_entries(e, o)
 
     async def test_get_many_raises(self):
-        with patch("aiokafka.AIOKafkaConsumer.getone") as mock:
-            mock.side_effect = asyncio.TimeoutError()
+        async with self.handler:
             with self.assertRaises(MinosHandlerNotFoundEnoughEntriesException):
-                await self.handler.get_many(count=3)
+                await self.handler.get_many(count=3, timeout=0.1)
+
+    async def _insert_one(self, instance):
+        async with aiopg.connect(**self.broker_queue_db) as connect:
+            async with connect.cursor() as cur:
+                await cur.execute(
+                    "INSERT INTO dynamic_queue (topic, partition_id, binary_data, creation_date) "
+                    "VALUES (%s, %s, %s, NOW()) "
+                    "RETURNING id;",
+                    (f"{instance.topic}Reply", 0, instance.value),
+                )
+                return (await cur.fetchone())[0]
 
     def _assert_equal_entries(self, expected, observed):
         self.assertEqual(expected.id, observed.id)
