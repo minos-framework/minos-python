2250e85c9bc5ad1f9502b34e666f6eb75178ce31
Sergio García Prado <sergio.garcia@clariteia.com>
2021-08-30 17:00:35 +0200

    ISSUE #556 * Simplify docstring.

--- minos/common/__init__.py
+++ minos/common/__init__.py
@@ -1,10 +1,4 @@
-"""
-Copyright (C) 2021 Clariteia SL
-
-This file is part of minos framework.
-
-Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
-"""
+"""minos.common module."""
 
 __version__ = "0.1.11"
 
