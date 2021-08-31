16606e6d0bedc1783df12bd4859aea355ec94f04
Sergio García Prado <sergio.garcia@clariteia.com>
2021-08-31 11:34:46 +0200

    ISSUE #21 * Update docstring (2).

--- minos/cqrs/__init__.py
+++ minos/cqrs/__init__.py
@@ -1,10 +1,4 @@
-"""
-Copyright (C) 2021 Clariteia SL
-
-This file is part of minos framework.
-
-Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
-"""
+"""minos.cqrs module."""
 
 __author__ = """Clariteia Devs"""
 __email__ = "devs@clariteia.com"
