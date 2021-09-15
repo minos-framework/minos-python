87908a8a4e8c4b66552c5d562c86baee26932bef
Alberto Amigo Alonso <albamigh@gmail.com>
2021-09-15 15:13:05 +0200

    Kong integration.
    issue #374

---
+++ minos/networks/discovery/clients/kong.py
@@ -0,0 +1,28 @@
+from .abc import DiscoveryClient
+
+
+class KongDiscovery(DiscoveryClient):
+    async def subscribe(
+        self,
+        host: str,
+        port: int,
+        name: str,
+        endpoints: list[dict[str, str]],
+        retry_tries: int = 3,
+        retry_delay: float = 5
+    ) -> None:
+        endpoint = f"{self.route}/services"
+        service_metadata = {
+            "name": name,
+            "url": f"http://{host}:{port}",
+        }
+        await self._rest_subscribe(endpoint, service_metadata, host, port, name, endpoints, retry_tries, retry_delay)
+
+        endpoint = f"{self.route}/{name}/routes"
+        service_metadata = {
+            "paths[]": ",".join([endpoint['url'] for endpoint in endpoints]),
+        }
+        await self._rest_subscribe(endpoint, service_metadata, host, port, name, endpoints, retry_tries, retry_delay)
+
+    async def unsubscribe(self, name: str, retry_tries: int = 3, retry_delay: float = 5) -> None:
+        pass
