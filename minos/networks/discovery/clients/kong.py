0fc67c9ff32c3c0e1dc466322526a326e8c252fd
Alberto Amigo Alonso <albamig@users.noreply.github.com>
2021-09-16 11:09:51 +0200

    Update minos/networks/discovery/clients/kong.py
    Co-authored-by: Sergio García Prado <sergio.garcia@clariteia.com>

--- minos/networks/discovery/clients/kong.py
+++ minos/networks/discovery/clients/kong.py
@@ -28,6 +28,13 @@ class KongDiscovery(DiscoveryClient):
         await self._rest_subscribe(endpoint, service_metadata, host, port, name, endpoints, retry_tries, retry_delay)
 
     async def unsubscribe(self, name: str, retry_tries: int = 3, retry_delay: float = 5) -> None:
+        """Perform an unsubscribe query.
+        
+        :param name: The name of the microservice to be unsubscribed.
+        :param retry_tries: Number of attempts before raising a failure exception.
+        :param retry_delay: Seconds to wait between attempts.
+        :return: This method does not return anything.
+        """
         endpoint = f"{self.route}/services/{name}"
 
         await self._rest_unsubscribe(endpoint, name, retry_tries, retry_delay)
