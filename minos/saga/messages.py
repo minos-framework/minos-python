f622721fae4fd2a3666a0c73d99512096d4318bf
Sergio García Prado <sergio.garcia@clariteia.com>
2021-10-04 16:26:44 +0200

    ISSUE #177 * Refactor `SagaRequest` and `SagaResponse`.

--- minos/saga/messages.py
+++ minos/saga/messages.py
@@ -1,25 +1,108 @@
-from minos.common import (
-    ModelType,
+from __future__ import (
+    annotations,
+)
+
+from enum import (
+    IntEnum,
+)
+from typing import (
+    Any,
+    Optional,
 )
 
 
-# noinspection PyPep8Naming
-def SagaRequest(*args, **kwargs) -> ModelType:
-    """Build a `SagaRequest` model type.
+class SagaRequest:
+    """Saga Request class."""
 
-    :param args: Additional positional parameters.
-    :param kwargs: Additional named parameters.
-    :return: A ``ModelType`` instance.
-    """
-    return ModelType.build("SagaRequest", *args, **kwargs)
+    __slots__ = (
+        "_target",
+        "_content",
+    )
+
+    def __init__(self, target: str, content: Any):
+        self._target = target
+        self._content = content
+
+    @property
+    def target(self) -> str:
+        """Get the target of the request.
+
+        :return: A ``str`` instance.
+        """
+        return self._target
+
+    # noinspection PyUnusedLocal
+    async def content(self, **kwargs) -> Any:
+        """Get the content of the request.
+
+        :param kwargs: Additional named parameters.
+        :return: The content of the request.
+        """
+        return self._content
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, type(self)) and self._target == other._target and self._content == other._content
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({self._target!r}, {self._content!r})"
+
+    def __hash__(self):
+        return hash((self._target, self._content))
 
 
-# noinspection PyPep8Naming
-def SagaResponse(*args, **kwargs) -> ModelType:
-    """Build a `SagaResponse` model type.
+class SagaResponse:
+    """Saga Response class."""
 
-    :param args: Additional positional parameters.
-    :param kwargs: Additional named parameters.
-    :return: A ``ModelType`` instance.
-    """
-    return ModelType.build("SagaResponse", *args, **kwargs)
+    __slots__ = (
+        "_content",
+        "_status",
+    )
+
+    def __init__(self, content: Any, status: Optional[SagaResponseStatus] = None):
+        if status is None:
+            status = SagaResponseStatus.SUCCESS
+
+        self._content = content
+        self._status = status
+
+    # noinspection PyUnusedLocal
+    async def content(self, **kwargs) -> Any:
+        """Get the response content.
+
+        :param kwargs: Additional named parameters.
+        :return: The content of the response.
+        """
+        return self._content
+
+    @property
+    def ok(self) -> bool:
+        """Check if the response is okay.
+
+        :return: ``True`` if the response is okay
+        """
+        return self._status == SagaResponseStatus.SUCCESS
+
+    @property
+    def status(self) -> SagaResponseStatus:
+        """Get the status code of the response.
+
+        :return: A ``ResponseStatus`` instance.
+        """
+        return self._status
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, type(self)) and self._content == other._content and self._status == other._status
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({self._content!r}, {self._status!r})"
+
+    def __hash__(self):
+        return hash((self._content, self._status))
+
+
+class SagaResponseStatus(IntEnum):
+    """Saga Response Status class."""
+
+    SUCCESS = 200
+    ERROR = 400
+    SYSTEM_ERROR = 500
--- tests/test_saga/test_messages.py
+++ tests/test_saga/test_messages.py
@@ -1,22 +1,62 @@
 import unittest
 
-from minos.common import (
-    ModelType,
-)
 from minos.saga import (
     SagaRequest,
     SagaResponse,
+    SagaResponseStatus,
 )
 
 
-class TestSagaRequest(unittest.TestCase):
-    def test_create(self):
-        self.assertEqual(ModelType.build("SagaRequest", {"foo": str}), SagaRequest({"foo": str}))
+class TestSagaRequest(unittest.IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        self.request = SagaRequest("UpdateProductPrice", 56)
+
+    def test_targetd(self):
+        self.assertEqual("UpdateProductPrice", self.request.target)
+
+    async def test_content(self):
+        self.assertEqual(56, await self.request.content())
+
+    def test_eq(self):
+        self.assertEqual(SagaRequest("UpdateProductPrice", 56), self.request)
+        self.assertNotEqual(SagaRequest("UpdateProductTitle", 56), self.request)
+        self.assertNotEqual(SagaRequest("UpdateProductPrice", 42), self.request)
+
+    def test_repr(self):
+        self.assertEqual("SagaRequest('UpdateProductPrice', 56)", repr(self.request))
+
+    def test_hash(self):
+        self.assertIsInstance(hash(self.request), int)
 
 
-class TestSagaResponse(unittest.TestCase):
-    def test_create(self):
-        self.assertEqual(ModelType.build("SagaResponse", {"foo": str}), SagaResponse({"foo": str}))
+class TestSagaResponse(unittest.IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        self.response = SagaResponse(56)
+
+    async def test_content(self):
+        self.assertEqual(56, await self.response.content())
+
+    def test_ok(self):
+        self.assertTrue(SagaResponse(56, SagaResponseStatus.SUCCESS).ok)
+        self.assertFalse(SagaResponse(56, SagaResponseStatus.ERROR).ok)
+
+    def test_default_status(self):
+        self.assertEqual(SagaResponseStatus.SUCCESS, self.response.status)
+
+    def test_status(self):
+        response = SagaResponse(56, SagaResponseStatus.SYSTEM_ERROR)
+        self.assertEqual(SagaResponseStatus.SYSTEM_ERROR, response.status)
+
+    def test_eq(self):
+        self.assertEqual(SagaResponse(56), self.response)
+        self.assertNotEqual(SagaResponse(42), self.response)
+        self.assertNotEqual(SagaResponse(56, SagaResponseStatus.SYSTEM_ERROR), self.response)
+
+    def test_repr(self):
+        self.assertEqual("SagaResponse(56, <SagaResponseStatus.SUCCESS: 200>)", repr(self.response))
+
+    def test_hash(self):
+        self.assertIsInstance(hash(self.response), int)
 
 
 if __name__ == "__main__":
