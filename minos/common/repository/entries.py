6c96cca90388cfa810c61f8ba4b7180c168a37ef
Sergio García Prado <sergio.garcia@clariteia.com>
2021-10-21 08:38:08 +0200

    ISSUE #619 * Add `RepositoryEntry.field_diff_container` property.

--- minos/common/repository/entries.py
+++ minos/common/repository/entries.py
@@ -25,6 +25,7 @@ if TYPE_CHECKING:
         Action,
         Aggregate,
         AggregateDiff,
+        FieldDiffContainer,
     )
 
 
@@ -75,6 +76,7 @@ class RepositoryEntry:
             aggregate_name=aggregate_diff.name,
             version=aggregate_diff.version,
             data=aggregate_diff.fields_diff.avro_bytes,
+            created_at=aggregate_diff.created_at,
             action=aggregate_diff.action,
         )
 
@@ -95,7 +97,6 @@ class RepositoryEntry:
         """
         from ..model import (
             AggregateDiff,
-            FieldDiffContainer,
         )
 
         return AggregateDiff(
@@ -104,8 +105,23 @@ class RepositoryEntry:
             self.version,
             self.action,
             self.created_at,
-            FieldDiffContainer() if not self.data else FieldDiffContainer.from_avro_bytes(self.data),
+            self.field_diff_container,
         )
+
+    @property
+    def field_diff_container(self) -> FieldDiffContainer:
+        """Get the stored field diff container.
+
+        :return: A ``FieldDiffContainer`` instance.
+        """
+        from ..model import (
+            FieldDiffContainer,
+        )
+
+        if not self.data:
+            return FieldDiffContainer.empty()
+
+        return FieldDiffContainer.from_avro_bytes(self.data)
 
     def __eq__(self, other: "RepositoryEntry") -> bool:
         return type(self) == type(other) and tuple(self) == tuple(other)
