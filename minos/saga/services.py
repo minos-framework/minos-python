6f950cb594e2ad5ec5017c279f4e2eb4748865df
Sergio García Prado <sergio.garcia@clariteia.com>
2021-11-15 13:09:53 +0100

    ISSUE #223 * Move transaction handlers to `minos.aggregate.TransactionService`.

--- minos/saga/services.py
+++ minos/saga/services.py
@@ -3,10 +3,6 @@ from dependency_injector.wiring import (
     inject,
 )
 
-from minos.aggregate import (
-    EventRepositoryConflictException,
-    TransactionRepository,
-)
 from minos.common import (
     MinosConfig,
 )
@@ -14,7 +10,6 @@ from minos.networks import (
     CommandReply,
     EnrouteDecorator,
     HandlerRequest,
-    ResponseException,
     enroute,
 )
 from minos.saga import (
@@ -24,48 +19,17 @@ from minos.saga import (
 
 
 class SagaService:
-    """Base Service class"""
+    """Saga Service class"""
 
+    # noinspection PyUnusedLocal
     @inject
-    def __init__(
-        self,
-        *args,
-        config: MinosConfig = Provide["config"],
-        transaction_repository: TransactionRepository = Provide["transaction_repository"],
-        saga_manager: SagaManager = Provide["saga_manager"],
-        **kwargs,
-    ):
-        self.config = config
-        self.transaction_repository = transaction_repository
+    def __init__(self, *args, saga_manager: SagaManager = Provide["saga_manager"], **kwargs):
         self.saga_manager = saga_manager
 
     @classmethod
     def __get_enroute__(cls, config: MinosConfig) -> dict[str, set[EnrouteDecorator]]:
         service_name = config.service.name
-        return {
-            cls.__reserve_transaction__.__name__: {enroute.broker.command(f"Reserve{service_name.title()}Transaction")},
-            cls.__reject_transaction__.__name__: {enroute.broker.command(f"Reject{service_name.title()}Transaction")},
-            cls.__commit_transaction__.__name__: {enroute.broker.command(f"Commit{service_name.title()}Transaction")},
-            cls.__saga_reply__.__name__: {enroute.broker.command(f"{service_name}Reply")},
-        }
-
-    async def __reserve_transaction__(self, request: HandlerRequest) -> None:
-        uuid = await request.content()
-        transaction = await self.transaction_repository.get(uuid)
-        try:
-            await transaction.reserve()
-        except EventRepositoryConflictException:
-            raise ResponseException("The transaction could not be reserved.")
-
-    async def __reject_transaction__(self, request: HandlerRequest) -> None:
-        uuid = await request.content()
-        transaction = await self.transaction_repository.get(uuid)
-        await transaction.reject()
-
-    async def __commit_transaction__(self, request: HandlerRequest) -> None:
-        uuid = await request.content()
-        transaction = await self.transaction_repository.get(uuid)
-        await transaction.commit()
+        return {cls.__saga_reply__.__name__: {enroute.broker.command(f"{service_name}Reply")}}
 
     async def __saga_reply__(self, request: HandlerRequest) -> None:
         raw: CommandReply = request.raw
