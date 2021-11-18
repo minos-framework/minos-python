dc07c371e9016492739d8206902f9d7f0545d554
Sergio García Prado <sergio.garcia@clariteia.com>
2021-11-18 15:18:05 +0100

    ISSUE #57 * Integrate `ModelRefResolver`.

--- minos/cqrs/handlers.py
+++ minos/cqrs/handlers.py
@@ -2,22 +2,17 @@ from __future__ import (
     annotations,
 )
 
+import logging
 from typing import (
     TypeVar,
 )
+
 from minos.aggregate import (
     AggregateDiff,
-    ModelRefExtractor,
-    ModelRefInjector,
+    ModelRefResolver,
 )
-from minos.common import (
-    Model,
-)
-from minos.networks import (
-    CommandBroker,
-    DynamicHandler,
-    DynamicHandlerPool,
-)
+
+logger = logging.getLogger(__name__)
 
 
 class PreEventHandler:
@@ -25,35 +20,21 @@ class PreEventHandler:
 
     @classmethod
     async def handle(cls, diff: T, resolve_references: bool = True, **kwargs) -> T:
+        """TODO
+
+        :param diff: TODO
+        :param resolve_references: TODO
+        :param kwargs: TODO
+        :return: TODO
+        """
         if not isinstance(diff, AggregateDiff) or not resolve_references:
             return diff
 
         try:
-            return await cls._query(diff, **kwargs)
-        except Exception:
+            return await ModelRefResolver(**kwargs).resolve(diff)
+        except Exception as exc:
+            logger.warning(f"An exception was raised while trying to resolve model references: {exc!r}")
             return diff
-
-    # noinspection PyUnusedLocal
-    @classmethod
-    async def _query(
-        cls, diff, dynamic_handler_pool: DynamicHandlerPool, command_broker: CommandBroker, **kwargs
-    ) -> None:
-        missing = ModelRefExtractor(diff.fields_diff).build()
-
-        recovered = dict()
-        async with dynamic_handler_pool.acquire() as handler:
-            for name, uuids in missing.items():
-                await command_broker.send(data={"uuids": uuids}, topic=f"Get{name}s", reply_topic=handler.topic)
-                for model in await cls._get_response(handler):
-                    recovered[model.uuid] = model
-
-        return ModelRefInjector(diff, recovered).build()
-
-    @staticmethod
-    async def _get_response(handler: DynamicHandler, **kwargs) -> list[Model]:
-        handler_entry = await handler.get_one(**kwargs)
-        response = handler_entry.data
-        return response.data
 
 
 T = TypeVar("T")
