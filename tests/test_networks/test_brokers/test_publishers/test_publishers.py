e1ff701b2b47462cf465d55934821117bf8b6bab
Sergio García Prado <sergio.garcia@clariteia.com>
2021-11-19 12:21:27 +0100

    ISSUE #349 #446 #447 * Unify `BrokerPublisher` tests.

--- tests/test_networks/test_brokers/test_publishers/test_publishers.py
+++ tests/test_networks/test_brokers/test_publishers/test_publishers.py
@@ -3,7 +3,9 @@ from unittest.mock import (
     AsyncMock,
     call,
 )
+from uuid import uuid4
 
+from minos.common import Model
 from psycopg2.sql import (
     SQL,
 )
@@ -12,10 +14,10 @@ from minos.common.testing import (
     PostgresAsyncTestCase,
 )
 from minos.networks import (
-    BrokerPublisher,
+    BrokerPublisher, BrokerMessage, BrokerMessageStatus,
 )
 from tests.utils import (
-    BASE_PATH,
+    BASE_PATH, FakeModel,
 )
 
 
@@ -24,17 +26,102 @@ class TestBrokerPublisher(PostgresAsyncTestCase):
 
     def setUp(self) -> None:
         super().setUp()
-        self.broker = BrokerPublisher.from_config(self.config)
+        self.publisher = BrokerPublisher.from_config(self.config)
+
+    async def asyncSetUp(self):
+        await super().asyncSetUp()
+        await self.publisher.setup()
+
+    async def asyncTearDown(self):
+        await self.publisher.destroy()
+        await super().asyncTearDown()
+
+    def test_from_config_default(self):
+        self.assertIsInstance(BrokerPublisher.from_config(config=self.config), BrokerPublisher)
+
+    async def test_send_event(self):
+        mock = AsyncMock(return_value=56)
+        self.publisher.enqueue = mock
+
+        identifier = await self.publisher.send(FakeModel("Foo"), topic="fake")
+
+        self.assertEqual(56, identifier)
+        self.assertEqual(1, mock.call_count)
+
+        args = mock.call_args.args
+        self.assertEqual("fake", args[0])
+        expected = BrokerMessage("fake", FakeModel("Foo"), service_name="Order")
+        self.assertEqual(expected, Model.from_avro_bytes(args[1]))
+
+    async def test_send_command(self):
+        mock = AsyncMock(return_value=56)
+        self.publisher.enqueue = mock
+
+        saga = uuid4()
+
+        identifier = await self.publisher.send(FakeModel("foo"), "fake", saga=saga, reply_topic="ekaf")
+
+        self.assertEqual(56, identifier)
+        self.assertEqual(1, mock.call_count)
+
+        args = mock.call_args.args
+        self.assertEqual("fake", args[0])
+        expected = BrokerMessage("fake", FakeModel("foo"), saga=saga, reply_topic="ekaf", service_name="Order")
+        self.assertEqual(expected, Model.from_avro_bytes(args[1]))
+
+    async def test_send_command_with_user(self):
+        mock = AsyncMock(return_value=56)
+        self.publisher.enqueue = mock
+
+        saga = uuid4()
+        user = uuid4()
+
+        identifier = await self.publisher.send(FakeModel("foo"), "fake", saga=saga, reply_topic="ekaf", user=user)
+
+        self.assertEqual(56, identifier)
+        self.assertEqual(1, mock.call_count)
+
+        args = mock.call_args.args
+        self.assertEqual("fake", args[0])
+        expected = BrokerMessage(
+            "fake", FakeModel("foo"), saga=saga, reply_topic="ekaf", user=user, service_name="Order"
+        )
+        self.assertEqual(expected, Model.from_avro_bytes(args[1]))
+
+    async def test_send_command_reply(self):
+        mock = AsyncMock(return_value=56)
+        self.publisher.enqueue = mock
+
+        saga = uuid4()
+        reply_topic = "fakeReply"
+
+        identifier = await self.publisher.send(
+            FakeModel("foo"), saga=saga, topic=reply_topic, status=BrokerMessageStatus.SUCCESS
+        )
+
+        self.assertEqual(56, identifier)
+        self.assertEqual(1, mock.call_count)
+
+        args = mock.call_args.args
+        self.assertEqual(reply_topic, args[0])
+
+        expected = BrokerMessage(
+            reply_topic,
+            FakeModel("foo"),
+            saga=saga,
+            status=BrokerMessageStatus.SUCCESS,
+            service_name=self.config.service.name,
+        )
+        observed = Model.from_avro_bytes(args[1])
+        self.assertEqual(expected, observed)
 
     async def test_enqueue(self):
         query = SQL("INSERT INTO producer_queue (topic, data, action) VALUES (%s, %s, %s) RETURNING id")
 
         mock = AsyncMock(return_value=(56,))
+        self.publisher.submit_query_and_fetchone = mock
 
-        async with self.broker:
-            self.broker.submit_query_and_fetchone = mock
-
-            identifier = await self.broker.enqueue("test_topic", b"test")
+        identifier = await self.publisher.enqueue("test_topic", b"test")
 
         self.assertEqual(56, identifier)
         self.assertEqual(1, mock.call_count)
