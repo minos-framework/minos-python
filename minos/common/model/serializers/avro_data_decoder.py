dae6c3c28c7b3df8e2c52fcc7a99f64d35b2e15d
Sergio García Prado <sergio.garcia@clariteia.com>
2022-01-05 09:45:01 +0100

    ISSUE #672 * Share `AvroDataDecoder` instance during all the building process.

--- minos/common/model/serializers/avro_data_decoder.py
+++ minos/common/model/serializers/avro_data_decoder.py
@@ -246,12 +246,13 @@ class AvroDataDecoder:
 
         if isinstance(data, dict):
             with suppress(Exception):
-                decoded_data = {n: AvroDataDecoder(t).build(data.get(n, None)) for n, t in type_.type_hints.items()}
+                decoded_data = {n: self._cast_value(t, data.get(n, None)) for n, t in type_.type_hints.items()}
                 return type_(**decoded_data, additional_type_hints=type_.type_hints)
 
         with suppress(Exception):
+            # TODO: check if the condition is needed.
             decoded_data = data if isinstance(data, (list, tuple)) else (data,)
-            decoded_data = (AvroDataDecoder(t).build(d) for d, t in zip(decoded_data, type_.type_hints.values()))
+            decoded_data = (self._cast_value(t, d) for d, t in zip(decoded_data, type_.type_hints.values()))
             return type_(*decoded_data, additional_type_hints=type_.type_hints)
 
         raise DataDecoderTypeException(type_, data)
