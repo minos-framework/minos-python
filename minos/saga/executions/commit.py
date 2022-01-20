d050e09503c60a8635f8a5fac1948d5547c5af56
Sergio García Prado <sergio.garcia@clariteia.com>
2022-01-20 15:49:13 +0100

    ISSUE #248 * Be compatible with the new `DynamicHandler` api (4).

--- minos/saga/executions/commit.py
+++ minos/saga/executions/commit.py
@@ -18,8 +18,8 @@ from minos.networks import (
     BrokerMessageV1,
     BrokerMessageV1Payload,
     BrokerPublisher,
-    DynamicBroker,
-    DynamicBrokerPool,
+    Broker,
+    BrokerPool,
 )
 
 from .steps import (
@@ -39,7 +39,7 @@ class TransactionCommitter:
         self,
         execution_uuid: UUID,
         executed_steps: list[SagaStepExecution],
-        broker_pool: DynamicBrokerPool = Provide["broker_pool"],
+        broker_pool: BrokerPool = Provide["broker_pool"],
         broker_publisher: BrokerPublisher = Provide["broker_publisher"],
         **kwargs,
     ):
@@ -102,7 +102,7 @@ class TransactionCommitter:
         logger.info("Successfully rejected!")
 
     @staticmethod
-    async def _get_response(handler: DynamicBroker, count: int, **kwargs) -> bool:
+    async def _get_response(handler: Broker, count: int, **kwargs) -> bool:
         messages = [message async for message in handler.receive_many(count, **kwargs)]
         return all(message.ok for message in messages)
 
