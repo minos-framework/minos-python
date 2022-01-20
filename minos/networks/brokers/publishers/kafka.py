a75e525070ee94b354f43e663101653441fd78b1
Sergio García Prado <sergio.garcia@clariteia.com>
2022-01-20 13:32:13 +0100

    ISSUE #482 * Remove queue from `KafkaBrokerPublisher`.

--- minos/networks/brokers/publishers/kafka.py
+++ minos/networks/brokers/publishers/kafka.py
@@ -3,12 +3,6 @@ from __future__ import (
 )
 
 import logging
-from asyncio import (
-    CancelledError,
-    Queue,
-    create_task,
-    gather,
-)
 
 from aiokafka import (
     AIOKafkaProducer,
@@ -34,14 +28,10 @@ logger = logging.getLogger(__name__)
 class KafkaBrokerPublisher(BrokerPublisher):
     """TODO"""
 
-    def __init__(self, *args, broker_host: str, broker_port: int, concurrency: int = 4, **kwargs):
+    def __init__(self, *args, broker_host: str, broker_port: int, **kwargs):
         super().__init__(*args, **kwargs)
         self.broker_host = broker_host
         self.broker_port = broker_port
-
-        self._queue = Queue(maxsize=1)
-        self._consumers = list()
-        self._concurrency = concurrency
 
     @classmethod
     def _from_config(cls, config: MinosConfig, **kwargs) -> KafkaBrokerPublisher:
@@ -53,10 +43,8 @@ class KafkaBrokerPublisher(BrokerPublisher):
     async def _setup(self) -> None:
         await super()._setup()
         await self.client.start()
-        await self._create_consumers()
 
     async def _destroy(self) -> None:
-        await self._destroy_consumers()
         await self.client.stop()
         await super()._destroy()
 
@@ -67,40 +55,6 @@ class KafkaBrokerPublisher(BrokerPublisher):
         :return: TODO
         """
         logger.info(f"Producing {message!r} message...")
-        await self._queue.put(message)
-
-    async def _create_consumers(self):
-        while len(self._consumers) < self._concurrency:
-            self._consumers.append(create_task(self._consume()))
-
-    async def _destroy_consumers(self):
-        await self._queue.join()
-
-        for consumer in self._consumers:
-            consumer.cancel()
-        await gather(*self._consumers, return_exceptions=True)
-        self._consumers = list()
-
-        while not self._queue.empty():
-            # TODO
-            message = self._queue.get_nowait()  # noqa
-
-    async def _consume(self) -> None:
-        while True:
-            await self._consume_one()
-
-    async def _consume_one(self) -> None:
-        message = await self._queue.get()
-        try:
-            try:
-                await self._send(message)
-            except (CancelledError, Exception) as exc:
-                # TODO
-                raise exc
-        finally:
-            self._queue.task_done()
-
-    async def _send(self, message: BrokerMessage) -> None:
         await self.client.send_and_wait(message.topic, message.avro_bytes)
 
     @cached_property
