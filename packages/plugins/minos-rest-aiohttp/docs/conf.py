eb4a14c05689a488ba89ea6f12878dcc9471daa3
Restyled.io <commits@restyled.io>
2022-02-17 11:30:56 +0000

    Restyled by isort

--- packages/plugins/minos-rest-aiohttp/docs/conf.py
+++ packages/plugins/minos-rest-aiohttp/docs/conf.py
@@ -22,7 +22,11 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
-from minos import plugins
+import sphinx_rtd_theme
+
+from minos import (
+    plugins,
+)
 
 # -- General configuration ---------------------------------------------
 
@@ -33,7 +37,6 @@ from minos import plugins
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 
-import sphinx_rtd_theme
 
 extensions = [
     "sphinxcontrib.apidoc",
