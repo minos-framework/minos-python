9e82e80c121596605ee0196878f966b0d87fad1f
Sergio García Prado <sergio.garcia@clariteia.com>
2022-03-17 16:39:16 +0100

    ISSUE #150 * Increase coverage.

--- packages/core/minos-microservice-common/tests/test_common/test_injections/test_decorators.py
+++ packages/core/minos-microservice-common/tests/test_common/test_injections/test_decorators.py
@@ -5,8 +5,10 @@ from typing import (
 )
 
 from minos.common import (
+    DependencyInjector,
+    Inject,
     Injectable,
-    InjectableMixin,
+    InjectableMixin, NotProvidedException,
 )
 
 K = TypeVar("K")
@@ -45,6 +47,57 @@ class TestInjection(unittest.TestCase):
 
         self.assertTrue(issubclass(_FooBar, (_Foo, _Bar, InjectableMixin)))
         self.assertEqual("foo", _FooBar.get_injectable_name())
+
+
+@Inject()
+def _get_foo_sync(foo: _Foo) -> _Foo:
+    return foo
+
+
+@Inject()
+async def _get_foo_async(foo: _Foo) -> _Foo:
+    return foo
+
+
+class TestInject(unittest.IsolatedAsyncioTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.foo = _Foo(34)
+        self.injector = DependencyInjector(None, [self.foo])
+        self.injector.wire()
+
+    def tearDown(self) -> None:
+        self.injector.unwire()
+        super().tearDown()
+
+    def test_decorator_sync(self):
+        self.assertEqual(self.foo, _get_foo_sync())
+
+    def test_decorator_sync_arg(self):
+        another = _Foo(56)
+        self.assertEqual(another, _get_foo_sync(another))
+
+    def test_decorator_sync_kwarg(self):
+        another = _Foo(56)
+        self.assertEqual(another, _get_foo_sync(foo=another))
+
+    async def test_decorator_async(self):
+        self.assertEqual(self.foo, await _get_foo_async())
+
+    async def test_decorator_async_arg(self):
+        another = _Foo(56)
+        self.assertEqual(another, await _get_foo_async(another))
+
+    async def test_decorator_async_kwarg(self):
+        another = _Foo(56)
+        self.assertEqual(another, await _get_foo_async(foo=another))
+
+    def test_resolve_by_name(self):
+        self.assertEqual(self.foo, Inject.resolve_by_name("foo"))
+
+    def test_resolve_by_name_raises(self):
+        with self.assertRaises(NotProvidedException):
+            Inject.resolve_by_name("bar")
 
 
 if __name__ == "__main__":
