d0e8aa473e39e840440a2f5c380ee375c0f1915f
Andrea Mucci <andrea@clariteia.com>
2022-03-19 19:41:59 +0100

    removed playbooks

--- tutorials/stock-wallet/microservices/stocks/src/aggregates.py
+++ tutorials/stock-wallet/microservices/stocks/src/aggregates.py
@@ -5,18 +5,34 @@ from uuid import (
 from minos.aggregate import (
     Aggregate,
     RootEntity,
+    Entity
 )
-
 
 class Stocks(RootEntity):
     """Stocks RootEntity class."""
+    ticker: str
+    updated: str
 
 
 class StocksAggregate(Aggregate[Stocks]):
     """StocksAggregate class."""
 
     @staticmethod
-    async def create() -> UUID:
+    async def add_ticker_to_stock(ticker: str) -> UUID:
         """Create a new instance."""
-        stocks = await Stocks.create()
+        stocks = await Stocks.create(ticker=ticker, updated="Never")
         return stocks
+
+    @staticmethod
+    async def update_time_ticker(stock_uuid: str, datetime: str):
+        stock = Stocks.get(stock_uuid)
+        stock.updated = datetime
+        await stock.save()
+
+    @staticmethod
+    async def get_all_tickers():
+        all_stocks = Stocks.get_all()
+        tickers = []
+        for stock in all_stocks:
+            tickers.append({"uuid": stock.uuid, "ticker": stock.ticker, "updated": stock.updated})
+        return tickers
--- tutorials/stock-wallet/microservices/stocks/src/commands/services.py
+++ tutorials/stock-wallet/microservices/stocks/src/commands/services.py
@@ -1,3 +1,5 @@
+import logging
+
 from minos.cqrs import (
     CommandService,
 )
@@ -7,11 +9,17 @@ from minos.networks import (
     ResponseException,
     enroute,
 )
+from minos.aggregate import (
+    Event,
+)
 
 from ..aggregates import (
     StocksAggregate,
 )
+import arrow
+from alpha_vantage.timeseries import TimeSeries
 
+logger = logging.getLogger(__name__)
 
 class StocksCommandService(CommandService):
     """StocksCommandService class."""
@@ -29,3 +37,23 @@ class StocksCommandService(CommandService):
             return Response(stocks)
         except Exception as exc:
             raise ResponseException(f"An error occurred during Stocks creation: {exc}")
+
+    @enroute.broker.event("WalletUpdated.tickers.create")
+    async def set_stock_ticker(self, request: Request):
+        event: Event = await request.content()
+        for ticker in event['tickers']:
+            await StocksAggregate.add_ticker_to_stock(ticker['ticker'])
+
+    @enroute.periodic.event("* * * * *")
+    async def get_stock_values(self, request: Request):
+        tickers = StocksAggregate.get_all_tickers()
+        if len(tickers) > 0:
+            timeserie = TimeSeries(key="LPY6CZEYR6OIMRYA")
+            for ticker in tickers:
+                updated = ticker['updated']
+                if updated == "Never":
+                    time_now = arrow.utcnow().format('YYYY-MM-DD HH:mm:ss')
+                    StocksAggregate.update_time_ticker(ticker['uuid'], time_now)
+                data, metadata = await timeserie.get_intraday(ticker)
+                logger.warning(metadata)
+                logger.warning(data)
