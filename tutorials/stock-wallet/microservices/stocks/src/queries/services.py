ed4710aca63fe74e6ac9c4ba551dde9210d740e1
Andrea Mucci <andrea@clariteia.com>
2022-03-28 17:57:48 +0200

    commented stocks events update

--- tutorials/stock-wallet/microservices/stocks/src/queries/services.py
+++ tutorials/stock-wallet/microservices/stocks/src/queries/services.py
@@ -33,32 +33,32 @@ class StocksQueryService(QueryService):
         """
         raise ResponseException("Not implemented yet!")
 
-    @enroute.broker.event("StocksCreated")
-    async def stocks_created(self, request: Request) -> None:
-        """Handle the Stocks creation events.
-
-        :param request: A request instance containing the aggregate difference.
-        :return: This method does not return anything.
-        """
-        event: Event = await request.content()
-        print(event)
-
-    @enroute.broker.event("StocksUpdated")
-    async def stocks_updated(self, request: Request) -> None:
-        """Handle the Stocks update events.
-
-        :param request: A request instance containing the aggregate difference.
-        :return: This method does not return anything.
-        """
-        event: Event = await request.content()
-        print(event)
-
-    @enroute.broker.event("StocksDeleted")
-    async def stocks_deleted(self, request: Request) -> None:
-        """Handle the Stocks deletion events.
-
-        :param request: A request instance containing the aggregate difference.
-        :return: This method does not return anything.
-        """
-        event: Event = await request.content()
-        print(event)
+    # @enroute.broker.event("StocksCreated")
+    # async def stocks_created(self, request: Request) -> None:
+    #     """Handle the Stocks creation events.
+    #
+    #     :param request: A request instance containing the aggregate difference.
+    #     :return: This method does not return anything.
+    #     """
+    #     event: Event = await request.content()
+    #     print(event)
+    #
+    # @enroute.broker.event("StocksUpdated")
+    # async def stocks_updated(self, request: Request) -> None:
+    #     """Handle the Stocks update events.
+    #
+    #     :param request: A request instance containing the aggregate difference.
+    #     :return: This method does not return anything.
+    #     """
+    #     event: Event = await request.content()
+    #     print(event)
+    #
+    # @enroute.broker.event("StocksDeleted")
+    # async def stocks_deleted(self, request: Request) -> None:
+    #     """Handle the Stocks deletion events.
+    #
+    #     :param request: A request instance containing the aggregate difference.
+    #     :return: This method does not return anything.
+    #     """
+    #     event: Event = await request.content()
+    #     print(event)
