a20c17cca17609cfc7a0f355f3e130933b9afdd6
Restyled.io <commits@restyled.io>
2022-04-01 16:05:25 +0000

    Restyled by isort

--- packages/plugins/minos-discovery-kong/tests/test_kong/test_utils.py
+++ packages/plugins/minos-discovery-kong/tests/test_kong/test_utils.py
@@ -1,6 +1,9 @@
 import unittest
 
-from minos.plugins.kong.utils import Endpoint, PathPart
+from minos.plugins.kong.utils import (
+    Endpoint,
+    PathPart,
+)
 
 
 class TestKongDiscoveryUtils(unittest.IsolatedAsyncioTestCase):
