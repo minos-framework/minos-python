786790edf9a98946c8900d3aedd620f29c8b9339
Vladyslav Fenchak <vladyslav.fenchak@clariteia.com>
2022-04-07 22:25:52 +0200

    ISSUE #378

--- packages/plugins/minos-discovery-kong/tests/utils.py
+++ packages/plugins/minos-discovery-kong/tests/utils.py
@@ -1,10 +1,8 @@
 import random
-import socket
-import struct
 from pathlib import (
     Path,
 )
 
 BASE_PATH = Path(__file__).parent
 CONFIG_FILE_PATH = BASE_PATH / "test_config.yml"
-TEST_HOST = socket.inet_ntoa(struct.pack(">I", random.randint(1, 0xFFFFFFFF)))
+TEST_HOST = f"{random.randint(1,255)}.{random.randint(1,255)}.{random.randint(1,255)}.{random.randint(1,255)}"
