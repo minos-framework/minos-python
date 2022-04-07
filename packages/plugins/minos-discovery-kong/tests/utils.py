47120b44311aa32257565b6f6236d8a845e69d36
Vladyslav Fenchak <vladyslav.fenchak@clariteia.com>
2022-04-07 22:28:29 +0200

    ISSUE #378

--- packages/plugins/minos-discovery-kong/tests/utils.py
+++ packages/plugins/minos-discovery-kong/tests/utils.py
@@ -5,4 +5,5 @@ from pathlib import (
 
 BASE_PATH = Path(__file__).parent
 CONFIG_FILE_PATH = BASE_PATH / "test_config.yml"
-TEST_HOST = f"{random.randint(1,255)}.{random.randint(1,255)}.{random.randint(1,255)}.{random.randint(1,255)}"
+rand = random.randint(1, 255)
+TEST_HOST = f"{rand}.{rand}.{rand}.{rand}"
