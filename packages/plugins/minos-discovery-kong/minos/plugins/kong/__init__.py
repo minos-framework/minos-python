8c51a04c3279810b2dfbc11cc5131d125834fdf6
Sergio García Prado <sergio.garcia@clariteia.com>
2022-04-11 13:41:52 +0200

    Update packages/plugins/minos-discovery-kong/minos/plugins/kong/__init__.py

--- packages/plugins/minos-discovery-kong/minos/plugins/kong/__init__.py
+++ packages/plugins/minos-discovery-kong/minos/plugins/kong/__init__.py
@@ -2,8 +2,10 @@ __author__ = "Minos Framework Devs"
 __email__ = "hey@minos.run"
 __version__ = "0.1.0a1"
 
-from .discovery import (
+from .client import (
     KongClient,
+)
+from .discovery import (
     KongDiscoveryClient,
 )
 from .middleware import (
