62a060f327e15cda6da241343df6ecad47cf3b76
Sergio García Prado <sergio.garcia@clariteia.com>
2022-04-12 08:50:26 +0200

    ISSUE #98 * Increase coverage.

--- packages/core/minos-microservice-aggregate/tests/test_aggregate/test_exceptions.py
+++ packages/core/minos-microservice-aggregate/tests/test_aggregate/test_exceptions.py
@@ -3,12 +3,18 @@ import unittest
 from minos.aggregate import (
     AggregateException,
     AlreadyDeletedException,
+    Event,
+    EventRepositoryConflictException,
     EventRepositoryException,
     NotFoundException,
+    SnapshotRepositoryConflictException,
     SnapshotRepositoryException,
 )
 from minos.common import (
     MinosException,
+)
+from tests.utils import (
+    Car,
 )
 
 
@@ -16,11 +22,29 @@ class TestExceptions(unittest.TestCase):
     def test_base(self):
         self.assertTrue(issubclass(AggregateException, MinosException))
 
-    def test_repository(self):
+    def test_event(self):
         self.assertTrue(issubclass(EventRepositoryException, AggregateException))
+
+    def test_event_conflict(self):
+        message = "There was a conflict"
+        offset = 56
+        exception = EventRepositoryConflictException(message, offset)
+
+        self.assertIsInstance(exception, EventRepositoryException)
+        self.assertEqual(message, str(exception))
+        self.assertEqual(offset, exception.offset)
 
     def test_snapshot(self):
         self.assertTrue(issubclass(SnapshotRepositoryException, AggregateException))
+
+    def test_snapshot_conflict(self):
+        entity = Car(3, "red")
+        event = Event.from_root_entity(entity)
+        exception = SnapshotRepositoryConflictException(entity, event)
+
+        self.assertIsInstance(exception, SnapshotRepositoryException)
+        self.assertEqual(entity, exception.previous)
+        self.assertEqual(event, exception.event)
 
     def test_snapshot_not_found(self):
         self.assertTrue(issubclass(NotFoundException, SnapshotRepositoryException))
