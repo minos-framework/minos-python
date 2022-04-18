f5e60d08fcc543146526fd585a124a3f719e1d9f
Sergio García Prado <sergio.garcia@clariteia.com>
2022-04-18 13:18:27 +0200

    ISSUE #99 * Add initial implementation of the `LmdbDatabaseClient`.

--- packages/plugins/minos-database-lmdb/minos/plugins/lmdb/clients.py
+++ packages/plugins/minos-database-lmdb/minos/plugins/lmdb/clients.py
@@ -1,25 +1,136 @@
+from __future__ import (
+    annotations,
+)
+
+from pathlib import (
+    Path,
+)
 from typing import (
     Any,
-    AsyncIterator,
+    Optional,
+    Union,
+)
+
+from lmdb import (
+    Environment,
 )
 
 from minos.common import (
     DatabaseClient,
     DatabaseOperation,
+    MinosAvroDatabaseProtocol,
+    MinosBinaryProtocol,
+)
+
+from .operations import (
+    LmdbDatabaseOperation,
+    LmdbDatabaseOperationType,
 )
 
 
 class LmdbDatabaseClient(DatabaseClient):
     """TODO"""
 
+    _environment: Optional[Environment]
+
+    def __init__(
+        self,
+        path: Optional[Union[str, Path]] = None,
+        max_tables: int = 100,
+        map_size: int = int(1e9),
+        protocol: type[MinosBinaryProtocol] = MinosAvroDatabaseProtocol,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        if path is None:
+            path = ".lmdb"
+
+        self._path = path
+        self._max_tables = max_tables
+        self._map_size = map_size
+        self._protocol = protocol
+        self._tables = {}
+
+        self._prefetched = None
+
+        self._environment = None
+
+    async def _setup(self) -> None:
+        await super()._setup()
+        self._create_environment()
+
+    async def _destroy(self) -> None:
+        await super()._destroy()
+        self._close_environment()
+
+    def _create_environment(self) -> None:
+        self._environment = Environment(str(self._path), max_dbs=self._max_tables, map_size=self._map_size)
+
+    def _close_environment(self) -> None:
+        if self._environment is not None:
+            self._environment.close()
+
     async def _is_valid(self, **kwargs) -> bool:
-        pass
+        return True
 
     async def _reset(self, **kwargs) -> None:
-        pass
+        self._prefetched = None
+        self._environment.sync()
+
+    async def _fetch_all(self, *args, **kwargs) -> Any:
+        prefetched = self._prefetched
+        self._prefetched = None
+        yield prefetched
 
     async def _execute(self, operation: DatabaseOperation) -> None:
-        pass
+        if not isinstance(operation, LmdbDatabaseOperation):
+            raise ValueError(f"The operation must be a {LmdbDatabaseOperation!r} instance. Obtained: {operation!r}")
 
-    def _fetch_all(self, *args, **kwargs) -> AsyncIterator[Any]:
-        pass
+        mapper = {
+            LmdbDatabaseOperationType.CREATE: self._create,
+            LmdbDatabaseOperationType.READ: self._read,
+            LmdbDatabaseOperationType.UPDATE: self._update,
+            LmdbDatabaseOperationType.DELETE: self._delete,
+        }
+
+        fn = mapper[operation.type_]
+
+        fn(table=operation.table, key=operation.key, value=operation.value)
+
+    # noinspection PyUnusedLocal
+    def _create(self, table: str, key: str, value: Any, **kwargs) -> None:
+        table = self._get_table(table)
+        with self._environment.begin(write=True) as transaction:
+            encoded = self._protocol.encode(value)
+            transaction.put(key.encode(), encoded, db=table)
+
+    # noinspection PyUnusedLocal
+    def _read(self, table: str, key: str, **kwargs):
+        table = self._get_table(table)
+        with self._environment.begin(db=table) as transaction:
+            value_binary = transaction.get(key.encode())
+            if value_binary is not None:
+                value = self._protocol.decode(value_binary)
+            else:
+                value = None
+
+        self._prefetched = value
+
+    # noinspection PyUnusedLocal
+    def _delete(self, table: str, key: str, **kwargs) -> None:
+        table = self._get_table(table)
+        with self._environment.begin(write=True, db=table) as transaction:
+            transaction.delete(key.encode())
+
+    # noinspection PyUnusedLocal
+    def _update(self, table: str, key: str, value: Any, **kwargs) -> None:
+        table = self._get_table(table)
+        with self._environment.begin(write=True, db=table) as transaction:
+            encoded = self._protocol.encode(value)
+            transaction.put(key.encode(), encoded, db=table, overwrite=True)
+
+    def _get_table(self, table: str) -> Any:
+        if table not in self._tables:
+            self._tables[table] = self._environment.open_db(table.encode())
+        return self._tables[table]
--- packages/plugins/minos-database-lmdb/minos/plugins/lmdb/operations.py
+++ packages/plugins/minos-database-lmdb/minos/plugins/lmdb/operations.py
@@ -1,7 +1,33 @@
+from enum import (
+    Enum,
+)
+from typing import (
+    Any,
+    Optional,
+)
+
 from minos.common import (
     DatabaseOperation,
 )
 
 
+class LmdbDatabaseOperationType(str, Enum):
+    """TODO"""
+
+    CREATE = "create"
+    READ = "read"
+    UPDATE = "update"
+    DELETE = "delete"
+
+
 class LmdbDatabaseOperation(DatabaseOperation):
     """TODO"""
+
+    def __init__(
+        self, type_: LmdbDatabaseOperationType, table: str, key: str, value: Optional[Any] = None, *args, **kwargs
+    ):
+        super().__init__(*args, **kwargs)
+        self.type_ = type_
+        self.table = table
+        self.key = key
+        self.value = value
