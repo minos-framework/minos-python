305567cb44e37f66f7cd094c75e0f01ff49e17e6
Sergio García Prado <sergio.garcia@clariteia.com>
2022-04-19 09:01:07 +0200

    ISSUE #99 * Add tests for `LmdbSagaExecutionDatabaseOperationFactory`.

---
+++ packages/plugins/minos-database-lmdb/tests/test_lmdb/test_factories/test_saga/test_factories.py
@@ -0,0 +1,44 @@
+import unittest
+from uuid import (
+    uuid4,
+)
+
+from minos.plugins.lmdb import (
+    LmdbDatabaseOperation,
+    LmdbDatabaseOperationType,
+    LmdbSagaExecutionDatabaseOperationFactory,
+)
+from minos.saga import (
+    SagaExecutionDatabaseOperationFactory,
+)
+
+
+class TestLmdbSagaExecutionDatabaseOperationFactory(unittest.TestCase):
+    def test_is_subclass(self):
+        self.assertTrue(issubclass(LmdbSagaExecutionDatabaseOperationFactory, SagaExecutionDatabaseOperationFactory))
+
+    def test_build_store(self):
+        factory = LmdbSagaExecutionDatabaseOperationFactory()
+
+        operation = factory.build_store(uuid4(), foo="bar")
+        self.assertIsInstance(operation, LmdbDatabaseOperation)
+        self.assertEqual(LmdbDatabaseOperationType.CREATE, operation.type_)
+
+    def test_build_load(self):
+        pass
+        factory = LmdbSagaExecutionDatabaseOperationFactory()
+
+        operation = factory.build_load(uuid4())
+        self.assertIsInstance(operation, LmdbDatabaseOperation)
+        self.assertEqual(LmdbDatabaseOperationType.READ, operation.type_)
+
+    def test_build_delete(self):
+        factory = LmdbSagaExecutionDatabaseOperationFactory()
+
+        operation = factory.build_delete(uuid4())
+        self.assertIsInstance(operation, LmdbDatabaseOperation)
+        self.assertEqual(LmdbDatabaseOperationType.DELETE, operation.type_)
+
+
+if __name__ == "__main__":
+    unittest.main()
