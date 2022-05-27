3e90b6d347610000a855a80a707bc79539f02548
Sergio García Prado <sergio.garcia@clariteia.com>
2022-05-27 12:59:35 +0200

    ISSUE #481 * Working on initial structure.

--- packages/plugins/minos-database-sqlalchemy/minos/plugins/sqlalchemy/snapshots/tables.py
+++ packages/plugins/minos-database-sqlalchemy/minos/plugins/sqlalchemy/snapshots/tables.py
@@ -3,13 +3,36 @@ from __future__ import (
 )
 
 import logging
+from datetime import (
+    date,
+    datetime,
+    time,
+)
+from typing import (
+    Any,
+)
 
 from sqlalchemy import (
+    Boolean,
+    Column,
+    Date,
+    DateTime,
+    Float,
+    Integer,
+    MetaData,
+    String,
     Table,
+    Time,
+)
+from sqlalchemy.sql.type_api import (
+    TypeEngine,
 )
 
 from minos.aggregate import (
     Entity,
+)
+from minos.common import (
+    ModelType,
 )
 
 logger = logging.getLogger(__name__)
@@ -18,5 +41,48 @@ logger = logging.getLogger(__name__)
 class SqlAlchemySnapshotTableBuilder:
     """TODO"""
 
-    def build(self, type_: Entity) -> Table:
+    def build(self, *types: type[Entity]) -> dict[type[Entity], Table]:
         """TODO"""
+        ans = dict()
+
+        metadata = MetaData()
+
+        for type_ in types:
+            ans[type_] = self._build_one(type_, metadata)
+        return ans
+
+    def _build_one(self, type_: type[Entity], metadata: MetaData) -> Table:
+        mt = ModelType.from_model(type_)
+
+        columns = self._build_columns(mt.type_hints)
+
+        table = Table(mt.name, metadata, *columns)
+
+        return table
+
+    def _build_columns(self, type_hints: dict[str, Any]) -> list[Column]:
+        columns = list()
+        for name, type_ in type_hints.items():
+            column_type = self._build_column_type(type_)
+            column = Column(name, column_type)
+            columns.append(column)
+
+        return columns
+
+    def _build_column_type(self, type_: type) -> TypeEngine:
+        if type_ is str:
+            return String()
+        if type_ is int:
+            return Integer()
+        if type_ is bool:
+            return Boolean()
+        if type_ is datetime:
+            return DateTime()
+        if type_ is float:
+            return Float()
+        if type_ is date:
+            return Date()
+        if type_ is time:
+            return Time()
+
+        raise ValueError("TODO")
